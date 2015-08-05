2d86eca8732d1dc76f8b0dd99c5d6f40b95c0aef
Uladzislau Rezki <urezki@gmail.com>
2015-08-05 16:20:50 +0200

    sched: check pinned tasks before balance
    The problem is there are pinned tasks in the system
    which can not be migrated on another CPUs while performing
    load balancing. For example: 'kworker', 'ksoftirqd' and
    user space applications.
    
    In order to not disturb idle CPUs in that cases we check
    if CPU can be balanced before making actual decision
    to kick idle CPU and do nohz balance.
    
    This is done for power saving.
    
    Signed-off-by: Uladzislau Rezki <urezki@gmail.com>
    
    [ab123321]
    Update from Sony 51.1.A.3.159 kernel copyleft release.
    
    Signed-off-by: Artem Labazov <123321artyom@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4912,6 +4912,10 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 	if (!se) {
 		add_nr_running(rq, 1);
+
+		if (unlikely(p->nr_cpus_allowed == 1))
+			rq->nr_pinned_tasks++;
+
 		inc_rq_walt_stats(rq, p);
 	}
 
@@ -5005,6 +5009,10 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 	if (!se) {
 		sub_nr_running(rq, 1);
+
+		if (unlikely(p->nr_cpus_allowed == 1))
+			rq->nr_pinned_tasks--;
+
 		dec_rq_walt_stats(rq, p);
 	}
 
@@ -10888,6 +10896,22 @@ static inline bool nohz_kick_needed(struct rq *rq, int *type)
 
 	if (time_before(now, nohz.next_balance))
 		return false;
+
+	if (unlikely(rq->nr_pinned_tasks > 0)) {
+		int delta = rq->nr_running - rq->nr_pinned_tasks;
+
+		/*
+		 * Check if it is possible to "unload" this CPU in case
+		 * of having pinned/affine tasks. Do not disturb idle
+		 * core if one of the below condition is true:
+		 *
+		 * - there is one pinned task and it is not "current"
+		 * - all tasks are pinned to this CPU
+		 */
+		if (delta < 2)
+			if (current->nr_cpus_allowed > 1 || !delta)
+				return false;
+	}
 
 	if (rq->nr_running >= 2 &&
 	    (!energy_aware() || cpu_overutilized(cpu)))
