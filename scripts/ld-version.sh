8d3f777d05605d8aca5f7222f34c45e3325db500
Felix Fietkau <nbd@nbd.name>
2016-01-18 12:27:49 +0100

    Kbuild: don't hardcode path to awk in scripts/ld-version.sh
    On some systems /usr/bin/awk does not exist, or is broken. Find it via
    $PATH instead.
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- scripts/ld-version.sh
+++ scripts/ld-version.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/awk -f
+#!/bin/sh
 # extract linker version number from stdin and turn into single number
+exec awk '
 	{
 	gsub(".*\\)", "");
 	gsub(".*version ", "");
@@ -8,3 +9,4 @@
 	print a[1]*100000000 + a[2]*1000000 + a[3]*10000;
 	exit
 	}
+'
