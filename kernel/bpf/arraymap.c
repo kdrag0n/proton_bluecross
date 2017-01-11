7596fabb96b68bb533a8ea4b620595b60e1c5990
Michal Hocko <mhocko@suse.com>
2017-01-10 16:57:30 -0800

    bpf: do not use KMALLOC_SHIFT_MAX
    Commit 01b3f52157ff ("bpf: fix allocation warnings in bpf maps and
    integer overflow") has added checks for the maximum allocateable size.
    It (ab)used KMALLOC_SHIFT_MAX for that purpose.
    
    While this is not incorrect it is not very clean because we already have
    KMALLOC_MAX_SIZE for this very reason so let's change both checks to use
    KMALLOC_MAX_SIZE instead.
    
    The original motivation for using KMALLOC_SHIFT_MAX was to work around
    an incorrect KMALLOC_MAX_SIZE which could lead to allocation warnings
    but it is no longer needed since "slab: make sure that KMALLOC_MAX_SIZE
    will fit into MAX_ORDER".
    
    Link: http://lkml.kernel.org/r/20161220130659.16461-3-mhocko@kernel.org
    Signed-off-by: Michal Hocko <mhocko@suse.com>
    Acked-by: Christoph Lameter <cl@linux.com>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Andrey Konovalov <andreyknvl@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- kernel/bpf/arraymap.c
+++ kernel/bpf/arraymap.c
@@ -64,7 +64,7 @@ static struct bpf_map *array_map_alloc(union bpf_attr *attr)
 	    attr->map_flags & ~ARRAY_CREATE_FLAG_MASK)
 		return ERR_PTR(-EINVAL);
 
-	if (attr->value_size >= 1 << (KMALLOC_SHIFT_MAX - 1))
+	if (attr->value_size > KMALLOC_MAX_SIZE)
 		/* if value_size is bigger, the user space won't be able to
 		 * access the elements.
 		 */
