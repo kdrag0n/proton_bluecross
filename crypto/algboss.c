7dd7f810f844fd57a735c800078c3accd05ef936
Felix Fietkau <nbd@nbd.name>
2017-07-07 17:12:51 +0200

    kernel: prevent cryptomgr from pulling in useless extra dependencies for tests that are not run
    Reduces kernel size after LZMA by about 5k on MIPS
    
    lede-commit: 044c316167e076479a344c59905e5b435b84a77f
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- crypto/algboss.c
+++ crypto/algboss.c
@@ -248,12 +248,16 @@ static int cryptomgr_schedule_test(struct crypto_alg *alg)
 	type = alg->cra_flags;
 
 	/* This piece of crap needs to disappear into per-type test hooks. */
+#ifdef CONFIG_CRYPTO_MANAGER_DISABLE_TESTS
+	type |= CRYPTO_ALG_TESTED;
+#else
 	if (!((type ^ CRYPTO_ALG_TYPE_BLKCIPHER) &
 	      CRYPTO_ALG_TYPE_BLKCIPHER_MASK) && !(type & CRYPTO_ALG_GENIV) &&
 	    ((alg->cra_flags & CRYPTO_ALG_TYPE_MASK) ==
 	     CRYPTO_ALG_TYPE_BLKCIPHER ? alg->cra_blkcipher.ivsize :
 					 alg->cra_ablkcipher.ivsize))
 		type |= CRYPTO_ALG_TESTED;
+#endif
 
 	param->type = type;
 
