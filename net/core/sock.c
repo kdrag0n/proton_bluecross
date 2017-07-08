12412a316b6088560b159c4de71d8b2b40c1b477
Felix Fietkau <nbd@nbd.name>
2017-07-08 08:16:31 +0200

    debloat: add some debloat patches, strip down procfs and make O_DIRECT support optional, saves ~15K after lzma on MIPS
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- net/core/sock.c
+++ net/core/sock.c
@@ -1465,9 +1465,11 @@ void sk_destruct(struct sock *sk)
 
 static void __sk_free(struct sock *sk)
 {
+#ifdef CONFIG_SOCK_DIAG
 	if (unlikely(sk->sk_net_refcnt && sock_diag_has_destroy_listeners(sk)))
 		sock_diag_broadcast_destroy(sk);
 	else
+#endif
 		sk_destruct(sk);
 }
 
