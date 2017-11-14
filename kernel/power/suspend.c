02434a0b599de75ccd2474467c04ba4ae9fb2d14
Joe Maples <joe@frap129.org>
2017-11-14 17:48:03 -0500

    PM: Port fingprintd quick thaw from OnePlus
     - Check for android.hardware.biometrics.fingerprint@2.1-service.fpc
     instead of fingerprintd
     - Revise code to fit the Linux code style
    
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -333,6 +333,9 @@ void __weak arch_suspend_enable_irqs(void)
  *
  * This function should be called after devices have been suspended.
  */
+#ifdef CONFIG_QUICK_THAW_FINGERPRINTD
+extern void thaw_fingerprintd(void);
+#endif
 static int suspend_enter(suspend_state_t state, bool *wakeup)
 {
 	int error, last_dev;
@@ -418,6 +421,9 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 
  Platform_wake:
 	platform_resume_noirq(state);
+#ifdef CONFIG_QUICK_THAW_FINGERPRINTD
+	thaw_fingerprintd();
+#endif
 	dpm_resume_noirq(PMSG_RESUME);
 
  Platform_early_resume:
