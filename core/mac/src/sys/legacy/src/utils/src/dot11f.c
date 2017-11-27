1af22568b255889562ff9674480c8afbe08c0f62
Daniel Mentz <danielmentz@google.com>
2017-11-27 14:45:31 -0800

    Remove unnecessary NULL pointer checks
    Clang generated the following warning in various places:
    
    example.c:10:14: warning: address of array 'e->abc' will always evaluate
    to 'true' [-Wpointer-bool-conversion]
            if (e && e->abc) {
                  ~~ ~~~^~~
    
    This clang warning message has been generated by compiling the following
    example program:
    
    struct example {
    	char abc[10];
    };
    
    struct example ex = { "pointer" };
    
    int main(void) {
    	struct example *e = &ex;
    
    	if (e && e->abc) {
    		return 1;
    	}
    	return 0;
    }
    
    Bug: 69808817
    Change-Id: I596bb20e9231289ad1b6492e0dc6d7e9a29807e6
    Signed-off-by: Daniel Mentz <danielmentz@google.com>

--- core/mac/src/sys/legacy/src/utils/src/dot11f.c
+++ core/mac/src/sys/legacy/src/utils/src/dot11f.c
@@ -12404,22 +12404,14 @@ uint32_t dot11f_get_packed_iersn(tpAniSirGlobal pCtx,
 			break;
 		}
 		*pnNeeded += (pIe->akm_suite_count * 4);
-		if (pIe->RSN_Cap) {
-			*pnNeeded += 2;
-		} else {
-			break;
-		}
+		*pnNeeded += 2;
 		if (pIe->pmkid_count) {
 			*pnNeeded += 2;
 		} else {
 			break;
 		}
 		*pnNeeded += (pIe->pmkid_count * 16);
-		if (pIe->gp_mgmt_cipher_suite) {
-			*pnNeeded += 4;
-		} else {
-			break;
-		}
+		*pnNeeded += 4;
 		break;
 	}
 	return status;
@@ -20546,13 +20538,9 @@ uint32_t dot11f_pack_ie_rsn(tpAniSirGlobal pCtx,
 		DOT11F_MEMCPY(pCtx, pBuf, &(pSrc->akm_suites), (pSrc->akm_suite_count * 4));
 		*pnConsumed += (pSrc->akm_suite_count * 4);
 		pBuf += (pSrc->akm_suite_count * 4);
-		if (pSrc->RSN_Cap) {
-			DOT11F_MEMCPY(pCtx, pBuf, pSrc->RSN_Cap, 2);
-			*pnConsumed += 2;
-			pBuf += 2;
-		} else {
-			break;
-		}
+		DOT11F_MEMCPY(pCtx, pBuf, pSrc->RSN_Cap, 2);
+		*pnConsumed += 2;
+		pBuf += 2;
 		if (pSrc->pmkid_count) {
 			frameshtons(pCtx, pBuf, pSrc->pmkid_count, 0);
 			*pnConsumed += 2;
@@ -20563,13 +20551,9 @@ uint32_t dot11f_pack_ie_rsn(tpAniSirGlobal pCtx,
 		DOT11F_MEMCPY(pCtx, pBuf, &(pSrc->pmkid), (pSrc->pmkid_count * 16));
 		*pnConsumed += (pSrc->pmkid_count * 16);
 		pBuf += (pSrc->pmkid_count * 16);
-		if (pSrc->gp_mgmt_cipher_suite) {
-			DOT11F_MEMCPY(pCtx, pBuf, pSrc->gp_mgmt_cipher_suite, 4);
-			*pnConsumed += 4;
-			/* fieldsEndFlag = 1 */
-		} else {
-			break;
-		}
+		DOT11F_MEMCPY(pCtx, pBuf, pSrc->gp_mgmt_cipher_suite, 4);
+		*pnConsumed += 4;
+		/* fieldsEndFlag = 1 */
 		break;
 	}
 	(void)pCtx;
