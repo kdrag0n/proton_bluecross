d42abf0a5513b465153c1327274ea5869192657e
Srinivas Girigowda <sgirigow@codeaurora.org>
2017-12-29 16:33:23 +0530

    qcacld-3.0: Synchronize __con_mode_handler and __hdd_open
    Interface up runs in normal mode (wifi@1.0-service) and it gets
    preempted by the con_mode change handler when Monitor mode is
    invoked via user. Then again when the normal mode gets scheduled
    after the completion of Monitor mode, it tries to access contents
    freed during the con_mode change.
    
    The execution of __con_mode_handler and __hdd_open is
    synchronized with help of a mutex lock and an atomic variable
    
    Bug: 71056409
    Change-Id: Ia78c225a21e2c505ffae6b16647ba8f2c661bbbd
    CRs-Fixed: 2158683
    Signed-off-by: Srinivas Girigowda <sgirigow@codeaurora.org>

--- core/hdd/src/wlan_hdd_main.c
+++ core/hdd/src/wlan_hdd_main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012-2017 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2012-2018 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -2257,8 +2257,12 @@ static int __hdd_open(struct net_device *dev)
 		return -EBUSY;
 	}
 
-	mutex_lock(&hdd_init_deinit_lock);
+	if (qdf_atomic_read(&hdd_ctx->con_mode_flag)) {
+		hdd_err("con_mode_handler is in progress; Please try again.");
+		return -EBUSY;
+	}
 
+	mutex_lock(&hdd_init_deinit_lock);
 	hdd_start_driver_ops_timer(eHDD_DRV_OP_IFF_UP);
 
 	/*
@@ -2312,7 +2316,6 @@ static int __hdd_open(struct net_device *dev)
 		hdd_debug("Sending Start Lpass notification");
 		hdd_lpass_notify_start(hdd_ctx, adapter);
 	}
-
 
 err_hdd_hdd_init_deinit_lock:
 	hdd_stop_driver_ops_timer();
@@ -10251,7 +10254,7 @@ int hdd_wlan_startup(struct device *dev)
 		hdd_err("Failed to init ACS Skip timer");
 	qdf_spinlock_create(&hdd_ctx->acs_skip_lock);
 #endif
-
+	qdf_atomic_init(&hdd_ctx->con_mode_flag);
 	qdf_mc_timer_init(&hdd_ctx->tdls_source_timer,
 			  QDF_TIMER_TYPE_SW,
 			  wlan_hdd_change_tdls_mode,
@@ -12022,8 +12025,9 @@ static int __con_mode_handler(const char *kmessage, struct kernel_param *kp,
 	if (ret)
 		return ret;
 
+	qdf_atomic_set(&hdd_ctx->con_mode_flag, 1);
 	cds_set_load_in_progress(true);
-
+	mutex_lock(&hdd_init_deinit_lock);
 	ret = param_set_int(kmessage, kp);
 
 	if (!(is_con_mode_valid(con_mode))) {
@@ -12039,9 +12043,6 @@ static int __con_mode_handler(const char *kmessage, struct kernel_param *kp,
 		ret = 0;
 		goto reset_flags;
 	}
-
-	if (!cds_wait_for_external_threads_completion(__func__))
-		hdd_warn("Waiting for monitor mode: External threads are active");
 
 	/* ensure adapters are stopped */
 	hdd_stop_present_mode(hdd_ctx, curr_mode);
@@ -12100,9 +12101,14 @@ static int __con_mode_handler(const char *kmessage, struct kernel_param *kp,
 
 	hdd_info("Mode successfully changed to %s", kmessage);
 	ret = 0;
+	mutex_unlock(&hdd_init_deinit_lock);
+	qdf_atomic_set(&hdd_ctx->con_mode_flag, 0);
+	return ret;
 
 reset_flags:
 	cds_set_load_in_progress(false);
+	mutex_unlock(&hdd_init_deinit_lock);
+	qdf_atomic_set(&hdd_ctx->con_mode_flag, 0);
 	return ret;
 }
 
