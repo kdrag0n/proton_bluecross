fa63602453e0d8f0623d8040a640bc115877801a
Chris Redpath <chris.redpath@arm.com>
2018-02-27 15:29:09 +0000

    ANDROID: Move schedtune en/dequeue before schedutil update triggers
    CPU rq util updates happen when rq signals are updated as part of
    enqueue and dequeue operations. Doing these updates triggers a call to
    the registered util update handler, which takes schedtune boosting
    into account. Enqueueing the task in the correct schedtune group after
    this happens means that we will potentially not see the boost for an
    entire throttle period.
    
    Move the enqueue/dequeue operations for schedtune before the signal
    updates which can trigger OPP changes.
    
    Change-Id: I4236e6b194bc5daad32ff33067d4be1987996780
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    (cherry picked from commit 8d4cd07335d7520f710f5929779e1e6441005dfa)
    (cherry picked from commit eb4ca937baa7cc25b2888dd6b93bed53ba16cc6a)
    (cherry picked from commit 0864577d0e5d67d397ecd8e2301863cb24798817)
    (cherry picked from commit 6448f82b52bb5cfee458abf63403037cf76dd9ea)
    (cherry picked from commit 383e95ccbadc4b9f9e7ab0625fd80eb3ab1e015d)
    (cherry picked from commit a726c2a9b71302ee566f448ed5106e99376e21a1)
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4868,6 +4868,25 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	struct sched_entity *se = &p->se;
 #ifdef CONFIG_SMP
 	int task_new = flags & ENQUEUE_WAKEUP_NEW;
+
+	/*
+	 * Update SchedTune accounting.
+	 *
+	 * We do it before updating the CPU capacity to ensure the
+	 * boost value of the current task is accounted for in the
+	 * selection of the OPP.
+	 *
+	 * We do it also in the case where we enqueue a throttled task;
+	 * we could argue that a throttled task should not boost a CPU,
+	 * however:
+	 * a) properly implementing CPU boosting considering throttled
+	 *    tasks will increase a lot the complexity of the solution
+	 * b) it's not easy to quantify the benefits introduced by
+	 *    such a more complex solution.
+	 * Thus, for the time being we go for the simple solution and boost
+	 * also for throttled RQs.
+	 */
+	schedtune_enqueue_task(p, cpu_of(rq));
 #endif
 
 #ifdef CONFIG_SCHED_WALT
@@ -4923,26 +4942,6 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	}
 
 #ifdef CONFIG_SMP
-
-	/*
-	 * Update SchedTune accounting.
-	 *
-	 * We do it before updating the CPU capacity to ensure the
-	 * boost value of the current task is accounted for in the
-	 * selection of the OPP.
-	 *
-	 * We do it also in the case where we enqueue a throttled task;
-	 * we could argue that a throttled task should not boost a CPU,
-	 * however:
-	 * a) properly implementing CPU boosting considering throttled
-	 *    tasks will increase a lot the complexity of the solution
-	 * b) it's not easy to quantify the benefits introduced by
-	 *    such a more complex solution.
-	 * Thus, for the time being we go for the simple solution and boost
-	 * also for throttled RQs.
-	 */
-	schedtune_enqueue_task(p, cpu_of(rq));
-
 	if (!se) {
 		if (!task_new && !rq->rd->overutilized &&
 		    cpu_overutilized(rq->cpu)) {
@@ -4967,6 +4966,17 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	struct cfs_rq *cfs_rq;
 	struct sched_entity *se = &p->se;
 	int task_sleep = flags & DEQUEUE_SLEEP;
+
+#ifdef CONFIG_SMP
+	/*
+	 * Update SchedTune accounting
+	 *
+	 * We do it before updating the CPU capacity to ensure the
+	 * boost value of the current task is accounted for in the
+	 * selection of the OPP.
+	 */
+	schedtune_dequeue_task(p, cpu_of(rq));
+#endif
 
 	for_each_sched_entity(se) {
 		cfs_rq = cfs_rq_of(se);
@@ -5020,16 +5030,6 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	}
 
 #ifdef CONFIG_SMP
-
-	/*
-	 * Update SchedTune accounting
-	 *
-	 * We do it before updating the CPU capacity to ensure the
-	 * boost value of the current task is accounted for in the
-	 * selection of the OPP.
-	 */
-	schedtune_dequeue_task(p, cpu_of(rq));
-
 #endif /* CONFIG_SMP */
 
 	hrtick_update(rq);
