2c1fe455f34c698d8567545297352d7d95bd1523
Ionela Voinescu <ionela.voinescu@arm.com>
2018-04-27 18:38:00 +0100

    ANDROID: sched/fair: add idle state filter to prefer_idle case
    The CPU selection process for a prefer_idle task either minimizes or
    maximizes the CPU capacity for idle CPUs depending on the task being
    boosted or not.
    
    Given that we are iterating through all CPUs, additionally filter the
    choice by preferring a CPU in a more shallow idle state. This will
    provide both a faster wake-up for the task and higher energy efficiency,
    by allowing CPUs in deeper idle states to remain idle.
    
    Change-Id: Ic55f727a0c551adc0af8e6ee03de6a41337a571b
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7006,6 +7006,7 @@ retry:
 			unsigned long capacity_orig = capacity_orig_of(i);
 			unsigned long wake_util, new_util;
 			long spare_cap;
+			int idle_idx = INT_MAX;
 
 			cpumask_clear_cpu(i, &search_cpus);
 
@@ -7044,6 +7045,10 @@ retry:
 			 * enqueued here.
 			 */
 			spare_cap = capacity_orig - new_util;
+
+			if (idle_cpu(i))
+				idle_idx = idle_get_state_idx(cpu_rq(i));
+
 
 			/*
 			 * Case A) Latency sensitive tasks
@@ -7088,13 +7093,18 @@ retry:
 					schedstat_inc(this_rq()->eas_stats.fbt_pref_idle);
 
 					if (boosted &&
-					    capacity_orig <= target_capacity)
+					    capacity_orig < target_capacity)
 						continue;
 					if (!boosted &&
-					    capacity_orig >= target_capacity)
+					    capacity_orig > target_capacity)
+						continue;
+					if (capacity_orig == target_capacity &&
+					    sysctl_sched_cstate_aware &&
+					    best_idle_cstate <= idle_idx)
 						continue;
 
 					target_capacity = capacity_orig;
+					best_idle_cstate = idle_idx;
 					best_idle_cpu = i;
 					continue;
 				}
@@ -7170,8 +7180,6 @@ retry:
 			 * consumptions without affecting performance.
 			 */
 			if (idle_cpu(i)) {
-				int idle_idx = idle_get_state_idx(cpu_rq(i));
-
 				/*
 				 * Skip CPUs in deeper idle state, but only
 				 * if they are also less energy efficient.
