b0ac55b04449354774cc93f00ffe2c8052cc4a6b
Adek Maulana <adekzmaulana@gmail.com>
2018-05-22 03:24:10 +0700

    sdfat: push rcu_barrier() from deactivate_locked_super() to filesystems
    * https://github.com/torvalds/linux/commit/8c0a853
    
    Change-Id: Ibad1087b6d211b66903f07c4bd27d64553da7388
    Signed-off-by: Adek Maulana <adekzmaulana@gmail.com>
    Signed-off-by: khusika <khusikadhamar@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- fs/sdfat/sdfat.c
+++ fs/sdfat/sdfat.c
@@ -4957,6 +4957,11 @@ static int __init sdfat_init_inodecache(void)
 
 static void sdfat_destroy_inodecache(void)
 {
+	/*
+	 * Make sure all delayed rcu free inodes are flushed before we
+	 * destroy cache.
+	 */
+	rcu_barrier();
 	kmem_cache_destroy(sdfat_inode_cachep);
 }
 
