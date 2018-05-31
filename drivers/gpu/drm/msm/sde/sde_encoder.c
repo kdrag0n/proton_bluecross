b10c2306f69db924ad6378e4bdccddcd9c896675
Dhaval Patel <pdhaval@codeaurora.org>
2018-05-31 13:33:31 -0700

    drm/msm/sde: increase ahb bus vote on first frame
    Increase ahb bus vote on first frame after idle
    pc because that will program the complete mdss
    register space.
    
    Change-Id: I360d43656f753c70acb51395a978998412354b64
    Signed-off-by: Dhaval Patel <pdhaval@codeaurora.org>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/gpu/drm/msm/sde/sde_encoder.c
+++ drivers/gpu/drm/msm/sde/sde_encoder.c
@@ -3410,10 +3410,14 @@ static void _sde_encoder_kickoff_phys(struct sde_encoder_virt *sde_enc)
 
 	if (sde_enc->elevated_ahb_vote) {
 		priv = sde_enc->base.dev->dev_private;
-		sde_kms = to_sde_kms(priv->kms);
-		if (priv != NULL && sde_kms != NULL) {
-			sde_power_scale_reg_bus(&priv->phandle,
-				sde_kms->core_client, VOTE_INDEX_LOW, false);
+		if (priv != NULL) {
+			sde_kms = to_sde_kms(priv->kms);
+			if (sde_kms != NULL) {
+				sde_power_scale_reg_bus(&priv->phandle,
+						sde_kms->core_client,
+						VOTE_INDEX_LOW,
+						false);
+			}
 		}
 		sde_enc->elevated_ahb_vote = false;
 	}
