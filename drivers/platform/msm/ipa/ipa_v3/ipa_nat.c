d9b5ea30f7f262e4aa7e3a862fe08050c8a6b720
Mohammed Javid <mjavid@codeaurora.org>
2018-06-08 16:55:32 +0530

    msm: ipa3: Add mutex to prevent race condition
    There is a race condition between ipa3_nat_init_cmd
    and ipa_read_nat4. The two thread will R/W the critical
    global variables. This will result in race conditions
    and possibly buffer overread/ overwrite issues. Add code
    to prevent this race condition.
    
    Change-Id: I6bf9a837ae941cf3ad9413da6e44821916acf196
    Acked-by: Pooja Kumari <kumarip@qti.qualcomm.com>
    Signed-off-by: Mohammed Javid <mjavid@codeaurora.org>
    (cherry picked from commit ca000f02663e5572e760599a3273700183779c55)
    (cherry picked from commit 0181ddb9263725832a1b03ab28d50c8885c29ced)
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/platform/msm/ipa/ipa_v3/ipa_nat.c
+++ drivers/platform/msm/ipa/ipa_v3/ipa_nat.c
@@ -794,19 +794,24 @@ int ipa3_nat_init_cmd(struct ipa_ioc_v4_nat_init *init)
 
 	IPADBG("\n");
 
+	mutex_lock(&ipa3_ctx->nat_mem.dev.lock);
+
 	if (!ipa3_ctx->nat_mem.dev.is_mapped) {
 		IPAERR_RL("attempt to init %s before mmap\n",
 			ipa3_ctx->nat_mem.dev.name);
+		mutex_unlock(&ipa3_ctx->nat_mem.dev.lock);
 		return -EPERM;
 	}
 
 	if (init->tbl_index >= 1) {
 		IPAERR_RL("Unsupported table index %d\n", init->tbl_index);
+		mutex_unlock(&ipa3_ctx->nat_mem.dev.lock);
 		return -EPERM;
 	}
 
 	if (init->table_entries == 0) {
 		IPAERR_RL("Table entries is zero\n");
+		mutex_unlock(&ipa3_ctx->nat_mem.dev.lock);
 		return -EPERM;
 	}
 
@@ -817,6 +822,7 @@ int ipa3_nat_init_cmd(struct ipa_ioc_v4_nat_init *init)
 		IPAHAL_NAT_IPV4);
 	if (result) {
 		IPAERR_RL("Bad params for NAT base table\n");
+		mutex_unlock(&ipa3_ctx->nat_mem.dev.lock);
 		return result;
 	}
 
@@ -827,6 +833,7 @@ int ipa3_nat_init_cmd(struct ipa_ioc_v4_nat_init *init)
 		IPAHAL_NAT_IPV4);
 	if (result) {
 		IPAERR_RL("Bad params for NAT expansion table\n");
+		mutex_unlock(&ipa3_ctx->nat_mem.dev.lock);
 		return result;
 	}
 
@@ -837,6 +844,7 @@ int ipa3_nat_init_cmd(struct ipa_ioc_v4_nat_init *init)
 		IPAHAL_NAT_IPV4_INDEX);
 	if (result) {
 		IPAERR_RL("Bad params for index table\n");
+		mutex_unlock(&ipa3_ctx->nat_mem.dev.lock);
 		return result;
 	}
 
@@ -847,6 +855,7 @@ int ipa3_nat_init_cmd(struct ipa_ioc_v4_nat_init *init)
 		IPAHAL_NAT_IPV4_INDEX);
 	if (result) {
 		IPAERR_RL("Bad params for index expansion table\n");
+		mutex_unlock(&ipa3_ctx->nat_mem.dev.lock);
 		return result;
 	}
 
@@ -880,6 +889,7 @@ int ipa3_nat_init_cmd(struct ipa_ioc_v4_nat_init *init)
 	result = ipa3_nat_send_init_cmd(&cmd, false);
 	if (result) {
 		IPAERR("Fail to send NAT init immediate command\n");
+		mutex_unlock(&ipa3_ctx->nat_mem.dev.lock);
 		return result;
 	}
 
@@ -905,6 +915,8 @@ int ipa3_nat_init_cmd(struct ipa_ioc_v4_nat_init *init)
 				 ipa3_ctx->nat_mem.index_table_expansion_addr);
 
 	ipa3_ctx->nat_mem.dev.is_hw_init = true;
+	mutex_unlock(&ipa3_ctx->nat_mem.dev.lock);
+
 	IPADBG("return\n");
 	return 0;
 }
