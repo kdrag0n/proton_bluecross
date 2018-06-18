cf4ecda83034302a7c9d2ebfc88b41af0c7a372b
Sultan Alsawaf <sultanxda@gmail.com>
2018-06-18 23:25:57 +0000

    kernel: Add API to mark IRQs and kthreads as performance critical
    On devices with a CPU that contains heterogeneous cores (e.g., big.LITTLE),
    it can be beneficial to place some performance-critical IRQs and kthreads
    onto the performance CPU cluster in order to improve performance.
    
    This commit adds the following APIs:
    -kthread_run_perf_critical() to create and start a perf-critical kthread
    -IRQF_PERF_CRITICAL to schedule an IRQ and any threads it may have onto
     performance CPUs
    -PF_PERF_CRITICAL to mark a process (mainly kthread) as performance
     critical (this is used by kthread_run_perf_critical())
    
    In order to accomodate this new API, the following changes are made:
    -Performance-critical IRQs have their affinities reaffined upon exit from
     suspend (since the affinities are broken when non-boot CPUs are disabled)
    -Performance-critical IRQs and their threads have their affinities reset
     upon entering suspend, so that upon immediate suspend exit (when only the
     boot CPU is online), interrupts can be processed and interrupt threads
     can be scheduled onto an online CPU (otherwise we'd hit a kernel BUG)
    -Global cpu_lp_mask and cpu_perf_mask symbols are added to provide
     a cpumask of low-power and performance CPUs, respectively. This is
     set to the big.LITTLE layout used on msm8998 by default.
    -do_set_cpus_allowed() and __set_cpus_allowed_ptr() are modified to enforce
     a performance-critical kthread's affinity
    -Mark perf-critical IRQs with IRQD_AFFINITY_MANAGED so userspace can't mess
     with their affinity
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -33,6 +33,21 @@
 
 #include "smpboot.h"
 
+/*
+ * This assumes that half of the CPUs are little and that they have lower
+ * CPU numbers than the big CPUs (e.g., on an 8-core system, CPUs 0-3 would be
+ * little and CPUs 4-7 would be big).
+ */
+#define LITTLE_CPU_MASK	((1UL << (NR_CPUS / 2)) - 1)
+#define BIG_CPU_MASK	(((1UL << NR_CPUS) - 1) & ~LITTLE_CPU_MASK)
+static const unsigned long little_cluster_cpus = LITTLE_CPU_MASK;
+const struct cpumask *const cpu_lp_mask = to_cpumask(&little_cluster_cpus);
+EXPORT_SYMBOL(cpu_lp_mask);
+
+static const unsigned long big_cluster_cpus = BIG_CPU_MASK;
+const struct cpumask *const cpu_perf_mask = to_cpumask(&big_cluster_cpus);
+EXPORT_SYMBOL(cpu_perf_mask);
+
 /**
  * cpuhp_cpu_state - Per cpu hotplug state storage
  * @state:	The current cpu state
@@ -1070,7 +1085,15 @@ static int cpu_down_maps_locked(unsigned int cpu, enum cpuhp_state target)
 
 static int do_cpu_down(unsigned int cpu, enum cpuhp_state target)
 {
+	struct cpumask newmask;
 	int err;
+
+	cpumask_andnot(&newmask, cpu_online_mask, cpumask_of(cpu));
+
+	/* One big cluster CPU and one little cluster CPU must remain online */
+	if (!cpumask_intersects(&newmask, cpu_perf_mask) ||
+		!cpumask_intersects(&newmask, cpu_lp_mask))
+		return -EINVAL;
 
 	cpu_maps_update_begin();
 	err = cpu_down_maps_locked(cpu, target);
@@ -1272,6 +1295,8 @@ int freeze_secondary_cpus(int primary)
 	int cpu, error = 0;
 
 	cpu_maps_update_begin();
+	unaffine_perf_irqs();
+
 	if (!cpu_online(primary))
 		primary = cpumask_first(cpu_online_mask);
 	/*
@@ -1354,6 +1379,7 @@ void enable_nonboot_cpus(void)
 	arch_enable_nonboot_cpus_end();
 
 	cpumask_clear(frozen_cpus);
+	reaffine_perf_irqs();
 out:
 	cpu_maps_update_done();
 }
