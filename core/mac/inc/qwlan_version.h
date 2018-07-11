954878ba012f53032103261d33749a02a2d789bc
Srinivas Girigowda <sgirigow@codeaurora.org>
2018-07-11 15:14:42 -0700

    qcacld-3.0: Release 5.3.1.1Q.1
    Update driver version to 5.3.1.1Q.1.
    
    Bug: 111224736
    Change-Id: Ib41338b867dbedd78b0abd8a48d5c13858c63ce7
    Signed-off-by: Srinivas Girigowda <sgirigow@codeaurora.org>

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -44,6 +44,6 @@
 #define QWLAN_VERSION_EXTRA            "Q"
 #define QWLAN_VERSION_BUILD            1
 
-#define QWLAN_VERSIONSTR               "5.3.1.1Q"
+#define QWLAN_VERSIONSTR               "5.3.1.1Q.1"
 
 #endif /* QWLAN_VERSION_H */
