e85b8c7c9991cc35aa48b1c2a1a78aa511fdaa79
jordan Johnston <johnstonljordan@gmail.com>
2018-08-13 02:42:36 -0400

    arm64: Don't inline __const_udelay
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- arch/arm64/lib/delay.c
+++ arch/arm64/lib/delay.c
@@ -33,7 +33,7 @@ void __delay(unsigned long cycles)
 }
 EXPORT_SYMBOL(__delay);
 
-inline void __const_udelay(unsigned long xloops)
+ void __const_udelay(unsigned long xloops)
 {
 	unsigned long loops;
 
