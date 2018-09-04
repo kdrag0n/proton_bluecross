f1fa7b41b7888aa495d2fad097d5a58b843230ca
Ethan Chen <intervigil@gmail.com>
2018-09-03 23:37:48 -0700

    uapi: Export sockaddr_storage for userspace
    Change-Id: I3851f40de8481ae32d43df3d96fe1fb9988233ce
    (cherry picked from commit d57a863f0826590d6ea977707d89c6493bbc9147)
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- include/uapi/linux/socket.h
+++ include/uapi/linux/socket.h
@@ -18,4 +18,11 @@ struct __kernel_sockaddr_storage {
 				/* _SS_MAXSIZE value minus size of ss_family */
 } __attribute__ ((aligned(_K_SS_ALIGNSIZE)));	/* force desired alignment */
 
+
+#ifndef __KERNEL__
+/* This header is exposed as linux/socket.h, so it must provide definition
+ * for sockaddr_storage in place of include/linux/socket.h */
+#define sockaddr_storage __kernel_sockaddr_storage
+#endif
+
 #endif /* _UAPI_LINUX_SOCKET_H */
