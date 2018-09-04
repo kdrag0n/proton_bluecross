15bc4e0a2d80f10eba1187d3af43e46efec4119c
Junzhe Zou <jnzhezou@codeaurora.org>
2018-09-04 15:17:08 -0700

    msm: camera: isp: add acquire hw handler
    This change splits the acquire device function into acquire device and
    acquire HW function. Acquire device will only return device handle.
    Acquire HW will acquire the HW required for the usecase. There is also
    a check to ensure backward compatibility.
    
    Change-Id: I436cd91e099d64abe404661b2bb7dfc09161abb1
    Signed-off-by: Junzhe Zou <jnzhezou@codeaurora.org>
    Signed-off-by: Harsh Shah <harshs@codeaurora.org>
    (cherry picked from commit 2520a0e2277afb5e37deffd8d51413bed48383b7)
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/media/platform/msm/camera/cam_isp/cam_isp_context.h
+++ drivers/media/platform/msm/camera/cam_isp/cam_isp_context.h
@@ -16,6 +16,7 @@
 
 #include <linux/spinlock.h>
 #include <uapi/media/cam_isp.h>
+#include <uapi/media/cam_defs.h>
 
 #include "cam_context.h"
 #include "cam_isp_hw_mgr_intf.h"
@@ -159,6 +160,9 @@ struct cam_isp_context_state_monitor {
  *                             true, if context is rdi only context
  * @state_monitor_head:        Write index to the state monitoring array
  * @cam_isp_ctx_state_monitor: State monitoring array
+ * @hw_acquired:               Indicate whether HW resources are acquired
+ * @init_received:             Indicate whether init config packet is received
+ * @split_acquire:             Indicate whether a separate acquire is expected
  *
  */
 struct cam_isp_context {
@@ -183,6 +187,9 @@ struct cam_isp_context {
 	atomic64_t                       state_monitor_head;
 	struct cam_isp_context_state_monitor cam_isp_ctx_state_monitor[
 		CAM_ISP_CTX_STATE_MONITOR_MAX_ENTRIES];
+	bool                             hw_acquired;
+	bool                             init_received;
+	bool                             split_acquire;
 };
 
 /**
