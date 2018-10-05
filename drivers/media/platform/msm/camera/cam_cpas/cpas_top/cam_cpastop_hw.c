0de597e9713b8685c0e5c8b05dca0d87d1dce204
Trishansh Bhardwaj <tbhardwa@codeaurora.org>
2018-10-05 14:42:20 +0530

    msm: camera: cpas: new soc id for SDM712 target
    New soc id is assigned for sdm712 target. Add support
    for the same.
    
    Change-Id: If89ead70045a5009d9ac29062840326edbb68d82
    Signed-off-by: Trishansh Bhardwaj <tbhardwa@codeaurora.org>
    (cherry picked from commit c169c5110c82081f9e5f60c6aaca616f5c041e46)
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/media/platform/msm/camera/cam_cpas/cpas_top/cam_cpastop_hw.c
+++ drivers/media/platform/msm/camera/cam_cpas/cpas_top/cam_cpastop_hw.c
@@ -492,9 +492,10 @@ static int cam_cpastop_poweron(struct cam_hw_info *cpas_hw)
 		}
 	}
 
-	if ((soc_private && ((soc_private->soc_id == SDM710_SOC_ID) ||
+	if (soc_private && ((soc_private->soc_id == SDM710_SOC_ID) ||
+		(soc_private->soc_id == SDM712_SOC_ID) ||
 		((soc_private->soc_id == SDM670_SOC_ID) &&
-		(soc_private->hw_rev == SDM670_V1_1))))) {
+		(soc_private->hw_rev == SDM670_V1_1)))) {
 
 		struct cam_cpas_reg *reg_info;
 		int tcsr_index;
@@ -552,9 +553,10 @@ static int cam_cpastop_poweroff(struct cam_hw_info *cpas_hw)
 		}
 	}
 
-	if ((soc_private && ((soc_private->soc_id == SDM710_SOC_ID)
+	if (soc_private && ((soc_private->soc_id == SDM710_SOC_ID)
+		|| (soc_private->soc_id == SDM712_SOC_ID)
 		|| ((soc_private->soc_id == SDM670_SOC_ID) &&
-		(soc_private->hw_rev == SDM670_V1_1))))) {
+		(soc_private->hw_rev == SDM670_V1_1)))) {
 
 		struct cam_cpas_reg *reg_info;
 		int tcsr_index;
