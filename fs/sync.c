de6931e536955da948d801fe1645e837880e9e9f
Park Ju Hyung <qkrwngud825@gmail.com>
2018-10-16 05:18:19 +0900

    fs/sync: add blocking variant of sync() to be called within the kernel
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- fs/sync.c
+++ fs/sync.c
@@ -108,7 +108,7 @@ static void fdatawait_one_bdev(struct block_device *bdev, void *arg)
  * just write metadata (such as inodes or bitmaps) to block device page cache
  * and do not sync it on their own in ->sync_fs().
  */
-SYSCALL_DEFINE0(sync)
+void blocking_sync(void)
 {
 	int nowait = 0, wait = 1;
 
@@ -120,6 +120,11 @@ SYSCALL_DEFINE0(sync)
 	iterate_bdevs(fdatawait_one_bdev, NULL);
 	if (unlikely(laptop_mode))
 		laptop_sync_completion();
+}
+
+SYSCALL_DEFINE0(sync)
+{
+	blocking_sync();
 	return 0;
 }
 
