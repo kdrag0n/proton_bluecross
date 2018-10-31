7cad4b7a2d803038a7492e68ff9ffa4962f4a549
Sultan Alsawaf <sultanxda@gmail.com>
2018-10-30 23:52:14 -0700

    kernel: Increase Pixel launcher top-app boost duration to 1000 ms
    500 ms isn't enough to totally eliminate stuttering. Increase the boost
    duration to 1000 ms.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2963,8 +2963,8 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 	/* Boost CPU to the max for 500 ms when launcher becomes a top app */
 	if (!memcmp(tsk->comm, "s.nexuslauncher", sizeof("s.nexuslauncher")) &&
 		!memcmp(cgrp->kn->name, "top-app", sizeof("top-app")) && !ret) {
-		cpu_input_boost_kick_max(500);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
+		cpu_input_boost_kick_max(1000);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 1000);
 	}
 
 	put_task_struct(tsk);
