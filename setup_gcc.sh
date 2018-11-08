289b546581b454e52e97bb3be518dd5124609da0
kdrag0n <dragon@khronodragon.com>
2018-11-07 18:07:59 -0800

    setup_gcc: fix prefix detection and setting
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- setup_gcc.sh
+++ setup_gcc.sh
@@ -6,6 +6,14 @@ tc=/usr
 # Path to the root of the 32-bit GCC toolchain
 tc32=$HOME/toolchains/gcc32-8.x
 
+# Optional: target prefix of the 64-bit GCC toolchain
+# Leave blank for autodetection
+prefix=aarch64-linux-gnu-
+
+# Optional: target prefix of the 32-bit GCC toolchain
+# Leave blank for autodetection
+prefix32=
+
 # Number of parallel jobs to run
 # Do not remove, set to 1 for no parallelism.
 jobs=6
@@ -16,16 +24,16 @@ jobs=6
 # Load the shared helpers early to prevent duplication
 source helpers.sh
 
-gcc_bin=$tc_gcc/bin
-gcc32_bin=$tc_gcc32/bin
-[ -z $prefix_gcc ] && prefix_gcc=$(get_gcc_prefix $gcc_bin)
-[ -z $prefix_gcc32 ] && prefix_gcc32=$(get_gcc_prefix $gcc32_bin)
+gcc_bin=$tc/bin
+gcc32_bin=$tc32/bin
+[ -z $prefix ] && prefix=$(get_gcc_prefix $gcc_bin)
+[ -z $prefix32 ] && prefix32=$(get_gcc_prefix $gcc32_bin)
 
 export PATH=$gcc_bin:$gcc32_bin:$PATH
 
 MAKEFLAGS+=(
-    CROSS_COMPILE=$prefix_gcc
-    CROSS_COMPILE_ARM32=$prefix_gcc32
+    CROSS_COMPILE=$prefix
+    CROSS_COMPILE_ARM32=$prefix32
 
-    KBUILD_COMPILER_STRING="$(get_gcc_version ${prefix_gcc}gcc)"
+    KBUILD_COMPILER_STRING="$(get_gcc_version ${prefix}gcc)"
 )
