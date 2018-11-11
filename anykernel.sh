6cff93c237e6bb69f306ca50b24f295115a25880
kdrag0n <dragon@khronodragon.com>
2018-11-11 15:27:45 -0800

    AK2: initial edits for Proton kernel
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,21 +4,17 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=
-device.name5=
+device.name1=blueline
+device.name2=crosshatch
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-is_slot_device=0;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
 ramdisk_compression=auto;
 
 
@@ -27,33 +23,23 @@ ramdisk_compression=auto;
 . /tmp/anykernel/tools/ak2-core.sh;
 
 
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-chmod -R 750 $ramdisk/*;
-chmod -R 755 $ramdisk/sbin;
-chown -R root:root $ramdisk/*;
-
-
 ## AnyKernel install
 dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+rm -fr $ramdisk/overlay
 
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
+if [ -d $ramdisk/.backup ]; then
+  ui_print "  • Patching ramdisk..."
+  patch_cmdline "skip_override" "skip_override"
 
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
+  mv /tmp/anykernel/overlay $ramdisk
+  cp /system_root/init.rc $ramdisk/overlay
+  insert_line $ramdisk/overlay/init.rc "init.proton.rc" after "import /init.usb.rc" "import /init.proton.rc"
+else
+  patch_cmdline "skip_override" ""
+fi
 
 # end ramdisk changes
 
