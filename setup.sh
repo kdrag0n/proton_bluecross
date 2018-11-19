45bfcafac35ebe73a2f2a9246a3aa05d27825a0c
kdrag0n <dragon@khronodragon.com>
2018-11-18 16:46:20 -0800

    Use GCC as the primary compiler (setup scripts)
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- setup.sh
+++ setup.sh
@@ -1,21 +1,18 @@
 # Toolchain paths
 
-# Path to the root of the Clang toolchain
-tc_clang=$HOME/toolchains/clang-8.x
-
 # Path to the root of the 64-bit GCC toolchain
-tc_gcc=$HOME/toolchains/gcc-4.x
+tc=/usr
 
 # Path to the root of the 32-bit GCC toolchain
-tc_gcc32=$HOME/toolchains/gcc32-4.x
+tc32=$HOME/toolchains/gcc32-8.x
 
 # Optional: target prefix of the 64-bit GCC toolchain
 # Leave blank for autodetection
-prefix_gcc=aarch64-linux-android-
+prefix=aarch64-linux-gnu-
 
 # Optional: target prefix of the 32-bit GCC toolchain
 # Leave blank for autodetection
-prefix_gcc32=arm-linux-androideabi-
+prefix32=
 
 # Number of parallel jobs to run
 # Do not remove, set to 1 for no parallelism.
@@ -27,25 +24,16 @@ jobs=6
 # Load the shared helpers early to prevent duplication
 source helpers.sh
 
-gcc_bin=$tc_gcc/bin
-gcc32_bin=$tc_gcc32/bin
-clang_bin=$tc_clang/bin
-[ -z $prefix_gcc ] && prefix_gcc=$(get_gcc_prefix $gcc_bin)
-[ -z $prefix_gcc32 ] && prefix_gcc32=$(get_gcc_prefix $gcc32_bin)
+gcc_bin=$tc/bin
+gcc32_bin=$tc32/bin
+[ -z $prefix ] && prefix=$(get_gcc_prefix $gcc_bin)
+[ -z $prefix32 ] && prefix32=$(get_gcc_prefix $gcc32_bin)
 
-export LD_LIBRARY_PATH=$tc_clang/lib64:$LD_LIBRARY_PATH
-export PATH=$clang_bin:$PATH
+export PATH=$gcc_bin:$gcc32_bin:$PATH
 
-export CROSS_COMPILE=$gcc_bin/$prefix_gcc
-export CROSS_COMPILE_ARM32=$gcc32_bin/$prefix_gcc32
-export CLANG_TRIPLE=aarch64-linux-gnu-
 MAKEFLAGS+=(
-    CC=clang
-    O=out
-    CROSS_COMPILE=$gcc_bin/$prefix_gcc
-    CROSS_COMPILE_ARM32=$gcc32_bin/$prefix_gcc32
-    CLANG_TRIPLE=aarch64-linux-gnu-
+    CROSS_COMPILE=$prefix
+    CROSS_COMPILE_ARM32=$prefix32
 
-    HOSTCC=clang
-    KBUILD_COMPILER_STRING="$(get_clang_version clang)"
+    KBUILD_COMPILER_STRING="$(get_gcc_version ${prefix}gcc)"
 )
