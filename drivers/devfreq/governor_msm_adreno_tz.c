5484a1b73c591c4c9c6371bbcc245bc0804d9173
kdrag0n <dragon@khronodragon.com>
2018-11-22 18:04:59 -0800

    adreno_idler: convert to state notifier
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/devfreq/governor_msm_adreno_tz.c
+++ drivers/devfreq/governor_msm_adreno_tz.c
@@ -20,7 +20,7 @@
 #include <linux/ftrace.h>
 #include <linux/mm.h>
 #include <linux/msm_adreno_devfreq.h>
-#include <linux/display_state.h>
+#include <linux/state_notifier.h>
 #include <asm/cacheflush.h>
 #include <soc/qcom/scm.h>
 #include "governor.h"
@@ -69,8 +69,7 @@ static void do_partner_stop_event(struct work_struct *work);
 static void do_partner_suspend_event(struct work_struct *work);
 static void do_partner_resume_event(struct work_struct *work);
 
-/* Display and suspend state booleans */ 
-static bool display_on;
+/* Suspend state boolean */
 static bool suspended = false;
 
 static struct workqueue_struct *workqueue;
@@ -395,7 +394,7 @@ static int tz_get_target_freq(struct devfreq *devfreq, unsigned long *freq, u32 
 	 * Force to use & record as min freq when system has
 	 * entered pm-suspend or screen-off state.
 	 */
-	if (suspended || !display_on) {
+	if (suspended || state_suspended) {
 		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
 		return 0;
 	}
@@ -584,7 +583,6 @@ static int tz_suspend(struct devfreq *devfreq)
 	unsigned int scm_data[2] = {0, 0};
 
 	__secure_tz_reset_entry2(scm_data, sizeof(scm_data), priv->is_64);
-	display_on = is_display_on();
 	suspended = true;
 
 	priv->bin.total_time = 0;
@@ -631,7 +629,6 @@ static int tz_handler(struct devfreq *devfreq, unsigned int event, void *data)
 	case DEVFREQ_GOV_RESUME:
 		spin_lock(&suspend_lock);
 		suspend_time += suspend_time_ms();
-		display_on = is_display_on();
 		suspended = false;
 		/* Reset the suspend_start when gpu resumes */
 		suspend_start = 0;
