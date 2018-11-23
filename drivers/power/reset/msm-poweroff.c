b50adb39232dba1630b5a9e179d82316223816cb
kdrag0n <dragon@khronodragon.com>
2018-11-22 22:18:31 -0800

    msm-poweroff: disable download mode by default
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -71,7 +71,7 @@ static bool scm_deassert_ps_hold_supported;
 /* Download mode master kill-switch */
 static void __iomem *msm_ps_hold;
 static phys_addr_t tcsr_boot_misc_detect;
-static int download_mode = 1;
+static int download_mode;
 static struct kobject dload_kobj;
 
 #ifdef CONFIG_QCOM_DLOAD_MODE
