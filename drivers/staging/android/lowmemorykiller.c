22235ea2b1ef95aa87da0296b52cb7f27612052e
kdrag0n <dragon@khronodragon.com>
2018-12-08 15:24:54 -0800

    lowmemorykiller: reduce CPU and DDR boost to 100 ms
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -500,8 +500,8 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 
 	selected_oom_score_adj = min_score_adj;
 
-	cpu_input_boost_kick_max(250);
-	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 250);
+	cpu_input_boost_kick_max(100);
+	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
 
 	rcu_read_lock();
 	for_each_process(tsk) {
