0ce859a206f9e589dd33e28d4bf6b23cd0a704db
kdrag0n <dragon@khronodragon.com>
2018-12-07 18:53:59 -0800

    Revert "devfreq_boost: convert to simple max boost driver"
    This reverts commit 0fa9e4f8f357bfd836a23eeac453515f0f97bbbb.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- include/linux/devfreq_boost.h
+++ include/linux/devfreq_boost.h
@@ -23,19 +23,28 @@ enum df_device {
 struct boost_dev {
 	struct workqueue_struct *wq;
 	struct devfreq *df;
+	struct work_struct input_boost;
+	struct delayed_work input_unboost;
 	struct work_struct max_boost;
 	struct delayed_work max_unboost;
 	unsigned long abs_min_freq;
+	unsigned long boost_freq;
 	unsigned long max_boost_expires;
 	unsigned long max_boost_jiffies;
+	bool disable;
 	spinlock_t lock;
 };
 
 #ifdef CONFIG_DEVFREQ_BOOST
+void devfreq_boost_kick(enum df_device device);
 void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms);
 void devfreq_register_boost_device(enum df_device device, struct devfreq *df);
 struct boost_dev *devfreq_get_boost_dev(enum df_device device);
 #else
+static inline
+void devfreq_boost_kick(enum df_device device)
+{
+}
 static inline
 void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms)
 {
