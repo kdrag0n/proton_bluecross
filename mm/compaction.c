9b419668a97d4d5560d99f602909696f9d28b8c5
kdrag0n <dragon@khronodragon.com>
2018-12-08 17:00:34 -0800

    Revert "mm: compaction: boost CPU and DDR bus during background compaction"
    This reverts commit f3620fb2bd20c3bfd93f3de066359632b3b5ccef.
    
    Completely unnecessary.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -20,8 +20,6 @@
 #include <linux/kthread.h>
 #include <linux/freezer.h>
 #include <linux/page_owner.h>
-#include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 #include "internal.h"
 
 #ifdef CONFIG_COMPACTION
@@ -1915,12 +1913,6 @@ static void kcompactd_do_work(pg_data_t *pgdat)
 	trace_mm_compaction_kcompactd_wake(pgdat->node_id, cc.order,
 							cc.classzone_idx);
 	count_vm_event(KCOMPACTD_WAKE);
-
-	cpu_input_boost_kick();
-	devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
-#if defined(CONFIG_CPU_INPUT_BOOST_DEBUG) || defined(CONFIG_DEVFREQ_BOOST_DEBUG)
-	pr_info("kicked cpu boost and max cpubw boost for 100 ms\n");
-#endif
 
 	for (zoneid = 0; zoneid <= cc.classzone_idx; zoneid++) {
 		int status;
