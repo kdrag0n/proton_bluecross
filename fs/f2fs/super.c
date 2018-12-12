b0f91a5f0d717c4d27b110f68c7b203a427ed7c9
kdrag0n <dragon@khronodragon.com>
2018-12-11 20:27:22 -0800

    f2fs: add proper cleancache support
    Call cleancache_init_fs with the VFS super_block object when mounting,
    like filesystems are supposed to if they use cleancache. ext4 was used
    as a reference.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -26,6 +26,7 @@
 #include <linux/f2fs_fs.h>
 #include <linux/sysfs.h>
 #include <linux/quota.h>
+#include <linux/cleancache.h>
 
 #include "f2fs.h"
 #include "node.h"
@@ -3108,6 +3109,8 @@ skip_recovery:
 				cur_cp_version(F2FS_CKPT(sbi)));
 	f2fs_update_time(sbi, CP_TIME);
 	f2fs_update_time(sbi, REQ_TIME);
+
+	cleancache_init_fs(sb);
 	return 0;
 
 free_meta:
