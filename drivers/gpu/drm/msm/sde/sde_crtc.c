f73cf1121227e862c682d9f436febba4fe8bcdeb
kdrag0n <dragon@khronodragon.com>
2018-12-22 16:40:54 -0800

    drm/msm: sde: remove devfreq boost
    It's really not necessary. The governors are good enough to handle this
    on their own.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -21,7 +21,6 @@
 #include <linux/debugfs.h>
 #include <linux/ktime.h>
 #include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 #include <uapi/drm/sde_drm.h>
 #include <drm/drm_mode.h>
 #include <drm/drm_crtc.h>
@@ -3626,10 +3625,8 @@ void sde_crtc_commit_kickoff(struct drm_crtc *crtc,
 	SDE_ATRACE_BEGIN("crtc_commit");
 
 #ifdef CONFIG_CPU_INPUT_BOOST
-	if (time_before(jiffies, last_input_time + msecs_to_jiffies(5000))) {
+	if (time_before(jiffies, last_input_time + msecs_to_jiffies(5000)))
 		cpu_input_boost_kick_general(100);
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
-	}
 #endif
 
 	is_error = _sde_crtc_prepare_for_kickoff_rot(dev, crtc);
