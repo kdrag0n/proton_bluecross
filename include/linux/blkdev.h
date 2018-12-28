8d896f6050ba93a1afd2a01f12af504bdabfa41b
kdrag0n <dragon@khronodragon.com>
2018-12-27 23:17:05 -0800

    block: reduce default nr_requests to 32
    Lower latency, negligible impact on throughput in SQLite tests.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -39,7 +39,7 @@ struct blk_flush_queue;
 struct pr_ops;
 
 #define BLKDEV_MIN_RQ	4
-#define BLKDEV_MAX_RQ	128	/* Default maximum */
+#define BLKDEV_MAX_RQ	32	/* Default maximum */
 
 /*
  * Maximum number of blkcg policies allowed to be registered concurrently.
