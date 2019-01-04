a1d9de02dc0746c25e1b4f3abda6f2f95afe17d2
kdrag0n <dragon@khronodragon.com>
2019-01-03 22:12:23 -0800

    flasher: overlay: remove default profile settings
    Users can choose to use these profiles if desired. Otherwise, don't
    force non-stock settings on them.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- flasher/overlay/init.proton.sh
+++
@@ -1,5 +0,0 @@
-#!/system/bin/sh
-
-# Set default values if necessary
-grep -q "persist.proton.profile" /data/property/persistent_properties || setprop persist.proton.profile 0
-grep -q "persist.spectrum.profile" /data/property/persistent_properties || setprop persist.spectrum.profile 0
