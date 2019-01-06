fafbf13caf0ff378eb1583a76e5f2a2917894030
kdrag0n <dragon@khronodragon.com>
2019-01-05 21:56:52 -0800

    Revert "zram: allow zram to allocate CMA pages"
    This reverts commit 0cedc12d5b9f16d4f3b016ac57ba4224701d4e70.
    
    This causes issues after a longer uptime:
    [7251.434019] cma: cma_alloc: alloc failed, req-size: 69 pages, ret: -16
    [7251.434047] platform soc:qcom,ion:qcom,ion-heap@19: Fail to allocate buffer
    
    This likely works with zsmalloc and fails with z3fold because zsmalloc
    has been edited here but not z3fold. z3fold cannot even handle
    __GFP_HIGHMEM allocations, so it might have an issue with __GFP_CMA.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1153,15 +1153,14 @@ compress_again:
 				__GFP_KSWAPD_RECLAIM |
 				__GFP_NOWARN |
 				__GFP_HIGHMEM |
-				__GFP_MOVABLE |
-				__GFP_CMA,
+				__GFP_MOVABLE,
 				&handle);
 	if (ret < 0) {
 		zcomp_stream_put(zram->comp);
 		atomic64_inc(&zram->stats.writestall);
 		ret = zpool_malloc(zram->mem_pool, comp_len,
 				GFP_NOIO | __GFP_HIGHMEM |
-				__GFP_MOVABLE | __GFP_CMA,
+				__GFP_MOVABLE,
 				&handle);
 		if (ret == 0)
 			goto compress_again;
