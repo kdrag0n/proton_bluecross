f73ef0672b0a23b699ccda045270996c64fb418b
kdrag0n <dragon@khronodragon.com>
2019-01-09 17:14:45 -0800

    Revert "usb: pd_engine: Fix memory leaks in probe and destroy"
    This reverts commit 4965997f7bb635e783aa6b10069312fc64ffd570.
    
    This is an older version of the fix. Replace it with the newer one.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/usb/pd/pd_engine.c
+++ drivers/usb/pd/pd_engine.c
@@ -2051,14 +2051,6 @@ static int update_ext_vbus(struct notifier_block *self, unsigned long action,
 	return NOTIFY_OK;
 }
 
-static void usbpd_device_release(struct device *dev)
-{
-	/*
-	 * Empty function to silence WARN_ON() upon device_unregister on a
-	 * device with no release() function.
-	 */
-}
-
 static const unsigned int usbpd_extcon_cable[] = {
 	EXTCON_USB,
 	EXTCON_USB_HOST,
@@ -2100,7 +2092,6 @@ struct usbpd *usbpd_create(struct device *parent)
 
 	device_initialize(&pd->dev);
 	pd->dev.parent = parent;
-	pd->dev.release = usbpd_device_release;
 	dev_set_drvdata(&pd->dev, pd);
 
 	ret = dev_set_name(&pd->dev, "usbpd%d", num_pd_instances++);
@@ -2108,10 +2099,8 @@ struct usbpd *usbpd_create(struct device *parent)
 		goto free_pd;
 
 	ret = device_add(&pd->dev);
-	if (ret < 0) {
-		kfree_const(pd->dev.kobj.name);
+	if (ret < 0)
 		goto free_pd;
-	}
 
 	ret = pd_engine_debugfs_init(pd);
 	if (ret < 0)
@@ -2249,7 +2238,7 @@ del_wq:
 exit_debugfs:
 	pd_engine_debugfs_exit(pd);
 del_pd:
-	device_unregister(&pd->dev);
+	device_del(&pd->dev);
 free_pd:
 	num_pd_instances--;
 	vfree(pd->logbuffer);
@@ -2275,7 +2264,7 @@ void usbpd_destroy(struct usbpd *pd)
 	power_supply_put(pd->usb_psy);
 	destroy_workqueue(pd->wq);
 	pd_engine_debugfs_exit(pd);
-	device_unregister(&pd->dev);
+	device_del(&pd->dev);
 	num_pd_instances--;
 	vfree(pd->logbuffer);
 	kfree(pd);
