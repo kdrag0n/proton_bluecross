16fac297e4ab99888a360a0fa529e02eed3e5957
Park Ju Hyung <qkrwngud825@gmail.com>
2019-01-10 22:52:23 +0900

    f2fs: convert wakelock APIs to wakeup_source
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- fs/f2fs/gc.h
+++ fs/f2fs/gc.h
@@ -8,7 +8,7 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
-#include <linux/wakelock.h>
+#include <linux/pm_wakeup.h>
 
 #define GC_THREAD_MIN_WB_PAGES		1	/*
 						 * a threshold to determine
@@ -30,7 +30,7 @@
 struct f2fs_gc_kthread {
 	struct task_struct *f2fs_gc_task;
 	wait_queue_head_t gc_wait_queue_head;
-	struct wake_lock gc_wakelock;
+	struct wakeup_source gc_wakelock;
 
 	/* for gc sleep time */
 	unsigned int urgent_sleep_time;
