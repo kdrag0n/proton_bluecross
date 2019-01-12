9a94e3707a22544419e7da2f471b25e4982ca20f
kdrag0n <dragon@khronodragon.com>
2019-01-11 21:02:34 -0800

    Revert "crypto: msm: Don't bail out when debugfs creation fails"
    This reverts commit 2177732d0480f3cf5eec64b7858922db80a7d184.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/crypto/msm/qcrypto.c
+++ drivers/crypto/msm/qcrypto.c
@@ -125,10 +125,8 @@ struct crypto_stat {
 	u64 ahash_op_fail;
 };
 static struct crypto_stat _qcrypto_stat;
-#ifdef CONFIG_DEBUG_FS
 static struct dentry *_debug_dent;
 static char _debug_read_buf[DEBUG_MAX_RW_BUF];
-#endif
 static bool _qcrypto_init_assign;
 struct crypto_priv;
 struct qcrypto_req_control {
@@ -1149,7 +1147,6 @@ static void _qcrypto_cra_aead_aes_exit(struct crypto_aead *tfm)
 	ctx->ahash_aead_aes192_fb = NULL;
 }
 
-#ifdef CONFIG_DEBUG_FS
 static int _disp_stats(int id)
 {
 	struct crypto_stat *pstat;
@@ -1323,7 +1320,6 @@ static int _disp_stats(int id)
 				i, cp->cpu_req[i]);
 	return len;
 }
-#endif
 
 static void _qcrypto_remove_engine(struct crypto_engine *pengine)
 {
@@ -5461,7 +5457,6 @@ static struct platform_driver __qcrypto = {
 	},
 };
 
-#ifdef CONFIG_DEBUG_FS
 static int _debug_qcrypto;
 
 static int _debug_stats_open(struct inode *inode, struct file *file)
@@ -5550,16 +5545,15 @@ err:
 	debugfs_remove_recursive(_debug_dent);
 	return rc;
 }
-#endif
 
 static int __init _qcrypto_init(void)
 {
+	int rc;
 	struct crypto_priv *pcp = &qcrypto_dev;
 
-#ifdef CONFIG_DEBUG_FS
-	_qcrypto_debug_init();
-#endif
-
+	rc = _qcrypto_debug_init();
+	if (rc)
+		return rc;
 	INIT_LIST_HEAD(&pcp->alg_list);
 	INIT_LIST_HEAD(&pcp->engine_list);
 	init_llist_head(&pcp->ordered_resp_list);
@@ -5584,9 +5578,7 @@ static int __init _qcrypto_init(void)
 static void __exit _qcrypto_exit(void)
 {
 	pr_debug("%s Unregister QCRYPTO\n", __func__);
-#ifdef CONFIG_DEBUG_FS
 	debugfs_remove_recursive(_debug_dent);
-#endif
 	platform_driver_unregister(&__qcrypto);
 }
 
