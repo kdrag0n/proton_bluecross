25d68796cbbc7fa5888e8ad91de46f60e9f736e6
kdrag0n <dragon@khronodragon.com>
2019-01-12 15:39:15 -0800

    sched/tune: introduce direct set_stune_boost()
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -4055,6 +4055,7 @@ void cpufreq_remove_update_util_hook(int cpu);
 int do_stune_boost(char *st_name, int boost, int *slot);
 int do_stune_sched_boost(char *st_name, int *slot);
 int reset_stune_boost(char *st_name, int slot);
+int set_stune_boost(char *st_name, int boost, int *boost_default);
 #else /* !CONFIG_DYNAMIC_STUNE_BOOST */
 static inline int do_stune_boost(char *st_name, int boost, int *slot)
 {
@@ -4067,6 +4068,11 @@ static inline int do_stune_sched_boost(char *st_name, int *slot)
 }
 
 static inline int reset_stune_boost(char *st_name, int slot)
+{
+	return 0;
+}
+
+static inline int set_stune_boost(char *st_name, int boost, int *boost_default)
 {
 	return 0;
 }
