2d53f50d3c08673cf76257d009f59cb9dcc377bc
kdrag0n <dragon@khronodragon.com>
2019-01-16 18:28:50 -0800

    smb-lib: Remove unnecessary brackets for fast charge
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -867,9 +867,7 @@ static int set_sdp_current(struct smb_charger *chg, int icl_ua)
 
 #ifdef CONFIG_FORCE_FAST_CHARGE
 	if (force_fast_charge > 0 && icl_ua == USBIN_500MA)
-	{
 		icl_ua = USBIN_900MA;
-	}
 #endif
 
 	/* power source is SDP */
