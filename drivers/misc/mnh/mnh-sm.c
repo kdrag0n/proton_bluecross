0acffb637434b039032b2dea76ca60d573149830
kdrag0n <dragon@khronodragon.com>
2019-01-22 22:23:48 -0800

    misc: mnh: Demote state change work from SCHED_FIFO class
    Changing the state of Easel doesn't deserve to be priority 5 SCHED_FIFO.
    It's fine as a normal SCHED_OTHER process.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/misc/mnh/mnh-sm.c
+++ drivers/misc/mnh/mnh-sm.c
@@ -2599,7 +2599,6 @@ static struct miscdevice mnh_sm_miscdevice = {
 static int mnh_sm_probe(struct platform_device *pdev)
 {
 	struct device *dev;
-	struct sched_param param = { .sched_priority = 5 };
 
 	int error = 0;
 	int i;
@@ -2640,7 +2639,6 @@ static int mnh_sm_probe(struct platform_device *pdev)
 		error = -ENOMEM;
 		goto fail_probe_1;
 	}
-	sched_setscheduler(mnh_sm_dev->thread, SCHED_FIFO, &param);
 
 	/* initialize driver structures */
 	mutex_init(&mnh_sm_dev->lock);
