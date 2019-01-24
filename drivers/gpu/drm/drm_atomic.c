950db834cdc73b2c91d5b5ef1f4d782becae9fa0
kdrag0n <dragon@khronodragon.com>
2019-01-23 17:33:41 -0800

    drm/atomic: Only boost if last input was within 2500 ms
    This can save a lot of power when boosting is unnecessary, especially
    during video playback.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -1906,7 +1906,8 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) && time_before(jiffies,
+			last_input_jiffies + msecs_to_jiffies(2500))) {
 		cpu_input_boost_kick();
 		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 	}
