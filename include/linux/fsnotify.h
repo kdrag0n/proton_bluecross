59602631bccc34ea8eea3f0507c751138e0d465c
kdrag0n <dragon@khronodragon.com>
2019-01-31 20:25:58 -0800

    Revert "restrict device timing side channels"
    This reverts commit b5d6517f37b557502a66ea2573340c4a3da315f8.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- include/linux/fsnotify.h
+++ include/linux/fsnotify.h
@@ -180,9 +180,6 @@ static inline void fsnotify_access(struct file *file)
 	struct inode *inode = path->dentry->d_inode;
 	__u32 mask = FS_ACCESS;
 
-	if (is_sidechannel_device(inode))
-		return;
-
 	if (S_ISDIR(inode->i_mode))
 		mask |= FS_ISDIR;
 
@@ -200,9 +197,6 @@ static inline void fsnotify_modify(struct file *file)
 	struct path *path = &file->f_path;
 	struct inode *inode = path->dentry->d_inode;
 	__u32 mask = FS_MODIFY;
-
-	if (is_sidechannel_device(inode))
-		return;
 
 	if (S_ISDIR(inode->i_mode))
 		mask |= FS_ISDIR;
