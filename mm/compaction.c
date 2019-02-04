1383e467b078ae2cfb45e222935d0a5307632374
kdrag0n <dragon@khronodragon.com>
2019-02-03 17:19:12 -0800

    Revert "BACKPORT: mm: fix pageblock heuristic"
    This reverts commit 0e6dd1bc8b3d6f099dd38a798e88666e5f6c956e.
    
    Unnecessary. I don't trust this - it's an old commit missing from all
    newer kernels.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -1370,7 +1370,7 @@ static enum compact_result __compact_finished(struct zone *zone, struct compact_
 		 * other migratetype buddy lists.
 		 */
 		if (find_suitable_fallback(area, order, migratetype,
-					   true, cc->order, &can_steal) != -1)
+						true, &can_steal) != -1)
 			return COMPACT_SUCCESS;
 	}
 
