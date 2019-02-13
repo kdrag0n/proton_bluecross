93e191263020b09e5082489dc6ac4ab3f5563465
kdrag0n <dragon@khronodragon.com>
2019-02-12 22:26:08 -0800

    Revert "net: ipc_router_core: fix memleak with IPC_LOGGING disabled"
    This reverts commit 3ede99e65074707618f48f0efbca8d1f7070f87d.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- net/ipc_router/ipc_router_core.c
+++ net/ipc_router/ipc_router_core.c
@@ -3950,7 +3950,6 @@ static void debugfs_init(void) {}
  */
 static void *ipc_router_create_log_ctx(char *name)
 {
-#ifdef CONFIG_IPC_LOGGING
 	struct ipc_rtr_log_ctx *sub_log_ctx;
 
 	sub_log_ctx = kmalloc(sizeof(*sub_log_ctx), GFP_KERNEL);
@@ -3968,9 +3967,6 @@ static void *ipc_router_create_log_ctx(char *name)
 	INIT_LIST_HEAD(&sub_log_ctx->list);
 	list_add_tail(&sub_log_ctx->list, &log_ctx_list);
 	return sub_log_ctx->log_ctx;
-#else
-	return NULL;
-#endif
 }
 
 static void ipc_router_log_ctx_init(void)
