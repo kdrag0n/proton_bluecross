5165bf0c5b54efe912ea756502f50ed677000924
Sultan Alsawaf <sultan@kerneltoast.com>
2019-02-15 14:03:06 -0800

    random: fix inconsistent spinlock usage
    All users of the struct entropy_store spinlock use the irqsave spinlock variant.
    Spinlock users of the same lock should use be consistent in their use of a
    certain spinlock primitive, which makes add_interrupt_randomness()'s spinlock
    usage incorrect.
    
    Fix the inconsistency by converting add_interrupt_randomness()'s spinlocks to
    use the irqsave primitive.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1200,6 +1200,7 @@ void add_interrupt_randomness(int irq, int irq_flags)
 	__u64			ip;
 	unsigned long		seed;
 	int			credit = 0;
+	unsigned long		flags;
 
 	if (cycles == 0)
 		cycles = get_reg(fast_pool, regs);
@@ -1230,7 +1231,7 @@ void add_interrupt_randomness(int irq, int irq_flags)
 		return;
 
 	r = &input_pool;
-	if (!spin_trylock(&r->lock))
+	if (!spin_trylock_irqsave(&r->lock, flags))
 		return;
 
 	fast_pool->last = now;
@@ -1246,7 +1247,7 @@ void add_interrupt_randomness(int irq, int irq_flags)
 		__mix_pool_bytes(r, &seed, sizeof(seed));
 		credit = 1;
 	}
-	spin_unlock(&r->lock);
+	spin_unlock_irqrestore(&r->lock, flags);
 
 	fast_pool->count = 0;
 
