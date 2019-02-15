09cb737bb779411f99d88e5b433fb6fee6046e7c
kdrag0n <dragon@khronodragon.com>
2019-02-14 22:35:32 -0800

    flasher: Add Gaming Spectrum profile as turbo
    This profile is identical to performance but uses the performance GPU
    governor instead of msm-adreno-tz.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- flasher/overlay/init.proton.sh
+++ flasher/overlay/init.proton.sh
@@ -51,6 +51,7 @@ case "$1" in
 
 		# GPU
 		gpu_min 180000000
+		gpu_gov msm-adreno-tz
 		;;
 	'balanced')
 		# CPU: Little
@@ -74,6 +75,7 @@ case "$1" in
 
 		# GPU
 		gpu_min 180000000
+		gpu_gov msm-adreno-tz
 		;;
 	'performance')
 		# CPU: Little
@@ -97,8 +99,33 @@ case "$1" in
 
 		# GPU
 		gpu_min 342000000
+		gpu_gov msm-adreno-tz
+		;;
+	'turbo')
+		# CPU: Little
+		little_min 576000
+		little_max 1766400
+		little_boost 1516800
+		little_general_boost 1132800
+		little_gov_param hispeed_freq 1228800
+		# CPU: Big
+		big_max 2803200
+		big_boost 1363200
+		big_general_boost 1209600
+		big_gov_param hispeed_freq 1363200
+		# CPU: Governor
+		gov_param hispeed_load 15
+		gov_param iowait_boost_enable 1
+		# CPU: Boost
+		stune_boost 50
+		stune_general_boost 25
+		boost_duration 125
+
+		# GPU
+		gpu_min 342000000
+		gpu_gov performance
 		;;
 	*)
-		echo "Valid actions: [usb] usb_msc, [profiles] battery, balanced, performance"
+		echo "Valid actions: [usb] usb_msc, [profiles] battery, balanced, performance, turbo"
 		exit 1
 esac
