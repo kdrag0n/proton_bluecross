ffaf462e69d648045040895d302543fe1f6ea3d3
kdrag0n <dragon@khronodragon.com>
2019-02-16 21:43:52 -0800

    init: Update profiles for recent changes
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- flasher/overlay/init.proton.sh
+++ flasher/overlay/init.proton.sh
@@ -8,14 +8,11 @@ little_min() { echo $1 > /sys/module/cpu_input_boost/parameters/remove_input_boo
 big_min() { echo $1 > /sys/module/cpu_input_boost/parameters/remove_input_boost_freq_perf; }
 little_boost() { echo $1 > /sys/module/cpu_input_boost/parameters/input_boost_freq_lp; }
 big_boost() { echo $1 > /sys/module/cpu_input_boost/parameters/input_boost_freq_hp; }
-little_general_boost() { echo $1 > /sys/module/cpu_input_boost/parameters/general_boost_freq_lp; }
-big_general_boost() { echo $1 > /sys/module/cpu_input_boost/parameters/general_boost_freq_hp; }
 little_gov_param() { echo $2 > /sys/devices/system/cpu/cpu0/cpufreq/$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)/$1; }
 big_gov_param() { echo $2 > /sys/devices/system/cpu/cpu4/cpufreq/$(cat /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor)/$1; }
 gov_param() { little_gov_param $1 $2; big_gov_param $1 $2; }
 boost_duration() { echo $1 > /sys/module/cpu_input_boost/parameters/input_boost_duration; }
 stune_boost() { echo $1 > /sys/module/cpu_input_boost/parameters/dynamic_stune_boost; }
-stune_general_boost() { echo $1 > /sys/module/cpu_input_boost/parameters/general_stune_boost; }
 gpu_min() { echo $1 > /sys/class/kgsl/kgsl-3d0/devfreq/min_freq; }
 
 # Actions
@@ -33,20 +30,17 @@ case "$1" in
 		# CPU: Little
 		little_min 300000
 		little_max 1516800
-		little_boost 748800
-		little_general_boost 652800
+		little_boost 652800
 		little_gov_param hispeed_freq 0
 		# CPU: Big
 		big_max 1209600
-		big_boost 0
-		big_general_boost 0
+		big_boost 825600
 		big_gov_param hispeed_freq 0
 		# CPU: Governor
 		gov_param hispeed_load 100
 		gov_param iowait_boost_enable 0
 		# CPU: Boost
 		stune_boost 15
-		stune_general_boost 1
 		boost_duration 32
 
 		# GPU
@@ -57,20 +51,17 @@ case "$1" in
 		# CPU: Little
 		little_min 300000
 		little_max 1766400
-		little_boost 825600
-		little_general_boost 748800
+		little_boost 748800
 		little_gov_param hispeed_freq 0
 		# CPU: Big
 		big_max 2649600
-		big_boost 979200
-		big_general_boost 902400
+		big_boost 825600
 		big_gov_param hispeed_freq 0
 		# CPU: Governor
 		gov_param hispeed_load 90
 		gov_param iowait_boost_enable 0
 		# CPU: Boost
 		stune_boost 25
-		stune_general_boost 10
 		boost_duration 64
 
 		# GPU
@@ -81,20 +72,17 @@ case "$1" in
 		# CPU: Little
 		little_min 576000
 		little_max 1766400
-		little_boost 1516800
-		little_general_boost 1132800
+		little_boost 1132800
 		little_gov_param hispeed_freq 1228800
 		# CPU: Big
 		big_max 2803200
-		big_boost 1363200
-		big_general_boost 1209600
+		big_boost 1209600
 		big_gov_param hispeed_freq 1363200
 		# CPU: Governor
 		gov_param hispeed_load 15
 		gov_param iowait_boost_enable 1
 		# CPU: Boost
 		stune_boost 50
-		stune_general_boost 25
 		boost_duration 125
 
 		# GPU
@@ -106,19 +94,16 @@ case "$1" in
 		little_min 576000
 		little_max 1766400
 		little_boost 1516800
-		little_general_boost 1132800
 		little_gov_param hispeed_freq 1228800
 		# CPU: Big
 		big_max 2803200
 		big_boost 1363200
-		big_general_boost 1209600
 		big_gov_param hispeed_freq 1363200
 		# CPU: Governor
 		gov_param hispeed_load 15
 		gov_param iowait_boost_enable 1
 		# CPU: Boost
 		stune_boost 50
-		stune_general_boost 25
 		boost_duration 125
 
 		# GPU
