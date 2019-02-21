3d94ec413d284c93ee9edca212d620ca7cd65a47
kdrag0n <dragon@khronodragon.com>
2019-02-20 18:55:06 -0800

    state_notifier: Clean up unused and unnecessary cruft
    We don't use this. The only purpose of this driver in our setup is to
    provide a global state_suspended boolean.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/soc/qcom/state_notifier.c
+++ drivers/soc/qcom/state_notifier.c
@@ -1,5 +1,5 @@
 /*
- * State Notifier Driver
+ * State notifier driver
  *
  * Copyright (c) 2013-2017, Pranav Vashi <neobuddy89@gmail.com>
  *           (c) 2017, Joe Maples <joe@frap129.org>
@@ -14,103 +14,18 @@
 #include <linux/module.h>
 #include <linux/state_notifier.h>
 
-#define DEFAULT_SUSPEND_DEFER_TIME 	1
-#define STATE_NOTIFIER			"state_notifier"
-
-static unsigned int suspend_defer_time = DEFAULT_SUSPEND_DEFER_TIME;
-module_param_named(suspend_defer_time, suspend_defer_time, uint, 0664);
-static struct delayed_work suspend_work;
-static struct workqueue_struct *susp_wq;
-struct work_struct resume_work;
 bool state_suspended;
-module_param_named(state_suspended, state_suspended, bool, 0444);
-static bool suspend_in_progress;
-
-static BLOCKING_NOTIFIER_HEAD(state_notifier_list);
-
-/**
- *	state_register_client - register a client notifier
- *	@nb: notifier block to callback on events
- */
-int state_register_client(struct notifier_block *nb)
-{
-	return blocking_notifier_chain_register(&state_notifier_list, nb);
-}
-EXPORT_SYMBOL(state_register_client);
-
-/**
- *	state_unregister_client - unregister a client notifier
- *	@nb: notifier block to callback on events
- */
-int state_unregister_client(struct notifier_block *nb)
-{
-	return blocking_notifier_chain_unregister(&state_notifier_list, nb);
-}
-EXPORT_SYMBOL(state_unregister_client);
-
-/**
- *	state_notifier_call_chain - notify clients on state_events
- *	@val: Value passed unmodified to notifier function
- *	@v: pointer passed unmodified to notifier function
- *
- */
-int state_notifier_call_chain(unsigned long val, void *v)
-{
-	return blocking_notifier_call_chain(&state_notifier_list, val, v);
-}
-EXPORT_SYMBOL_GPL(state_notifier_call_chain);
-
-static void _suspend_work(struct work_struct *work)
-{
-	state_suspended = true;
-	state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
-	suspend_in_progress = false;
-}
-
-static void _resume_work(struct work_struct *work)
-{
-	state_suspended = false;
-	state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
-}
 
 void state_suspend(void)
 {
-	if (state_suspended || suspend_in_progress)
-		return;
-
-	suspend_in_progress = true;
-
-	queue_delayed_work(susp_wq, &suspend_work,
-		msecs_to_jiffies(suspend_defer_time * 1000));
+	state_suspended = true;
 }
 
 void state_resume(void)
 {
-	if (delayed_work_pending(&suspend_work))
-		cancel_delayed_work_sync(&suspend_work);
-	suspend_in_progress = false;
-
-	if (state_suspended)
-		queue_work(susp_wq, &resume_work);
+	state_suspended = false;
 }
-
-static int __init state_notifier_init(void)
-{
-	susp_wq =
-	    alloc_workqueue("state_susp_wq",
-			    WQ_HIGHPRI | WQ_UNBOUND | WQ_MEM_RECLAIM, 0);
-
-	if (!susp_wq)
-		pr_err("State Notifier failed to allocate suspend workqueue\n");
-
-	INIT_DELAYED_WORK(&suspend_work, _suspend_work);
-	INIT_WORK(&resume_work, _resume_work);
-
-	return 0;
-}
-
-subsys_initcall(state_notifier_init);
 
 MODULE_AUTHOR("Pranav Vashi <neobuddy89@gmail.com>");
-MODULE_DESCRIPTION("State Notifier Driver");
+MODULE_DESCRIPTION("Suspend state tracker");
 MODULE_LICENSE("GPLv2");
