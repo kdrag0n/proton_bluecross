1684d1479dd5e44852ab4a4e5664f72d80b668e8
Tyler Nijmeh <tylernij@gmail.com>
2019-02-20 18:02:05 -0800

    cpu_input_boost: Fix incorrect SchedTune boost location
    This unifies SchedTune boost with the cpufreq boosting code in the boost
    workers, preventing a race condition a possible race condition in theory
    as well as cleaning up the code.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -243,13 +243,13 @@ static void input_boost_worker(struct work_struct *work)
 	if (!cancel_delayed_work_sync(&b->input_unboost)) {
 		set_boost_bit(b, INPUT_BOOST);
 		update_online_cpu_policy();
+
+		update_stune_boost(b, state, INPUT_STUNE_BOOST,
+				   input_stune_boost, &b->input_stune_slot);
 	}
 
 	queue_delayed_work(b->wq, &b->input_unboost,
 			   msecs_to_jiffies(input_boost_duration));
-
-	update_stune_boost(b, state, INPUT_STUNE_BOOST, input_stune_boost,
-			   &b->input_stune_slot);
 }
 
 static void input_unboost_worker(struct work_struct *work)
@@ -272,13 +272,13 @@ static void max_boost_worker(struct work_struct *work)
 	if (!cancel_delayed_work_sync(&b->max_unboost)) {
 		set_boost_bit(b, MAX_BOOST);
 		update_online_cpu_policy();
+
+		update_stune_boost(b, state, MAX_STUNE_BOOST, max_stune_boost,
+				   &b->max_stune_slot);
 	}
 
 	queue_delayed_work(b->wq, &b->max_unboost,
 			   msecs_to_jiffies(atomic_read(&b->max_boost_dur)));
-
-	update_stune_boost(b, state, MAX_STUNE_BOOST, max_stune_boost,
-			   &b->max_stune_slot);
 }
 
 static void max_unboost_worker(struct work_struct *work)
@@ -301,13 +301,13 @@ static void general_boost_worker(struct work_struct *work)
 	if (!cancel_delayed_work_sync(&b->general_unboost)) {
 		set_boost_bit(b, GENERAL_BOOST);
 		update_online_cpu_policy();
+
+		update_stune_boost(b, state, GENERAL_STUNE_BOOST,
+				   general_stune_boost, &b->general_stune_slot);
 	}
 
 	queue_delayed_work(b->wq, &b->general_unboost,
 		msecs_to_jiffies(atomic_read(&b->general_boost_dur)));
-
-	update_stune_boost(b, state, GENERAL_STUNE_BOOST, general_stune_boost,
-		&b->general_stune_slot);
 }
 
 static void general_unboost_worker(struct work_struct *work)
