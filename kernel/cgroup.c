69f761865b33475791972f74bd227bc133493862
kdrag0n <dragon@khronodragon.com>
2019-02-20 19:22:08 -0800

    kernel: Only boost on fork if the screen is on
    This prevents unnecessary boosting when services and/or content
    providers start in the background to perform jobs while the device is
    not actively being used.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -65,6 +65,7 @@
 #include <linux/binfmts.h>
 #include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
+#include <linux/state_notifier.h>
 #include <net/sock.h>
 
 #define CREATE_TRACE_POINTS
@@ -2962,7 +2963,7 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 		ret = cgroup_attach_task(cgrp, tsk, threadgroup);
 
 	/* This covers boosting for app launches and app transitions */
-	if (!ret && !threadgroup &&
+	if (!ret && !threadgroup && !state_suspended &&
 		!memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
 		is_zygote_pid(tsk->parent->pid)) {
 		cpu_input_boost_kick_max(500);
