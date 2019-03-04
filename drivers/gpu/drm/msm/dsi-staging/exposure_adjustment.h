c9791bf906ac5f567739af0d895a3b9f10f476e2
kdrag0n <dragon@khronodragon.com>
2019-03-04 00:31:55 -0800

    drm/msm/dsi-staging: Bump smart ELVSS disabling threshold
    This is the correct value for C1's display.
    
    Suggested-by: DevriesL <therkduan@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/gpu/drm/msm/dsi-staging/exposure_adjustment.h
+++ drivers/gpu/drm/msm/dsi-staging/exposure_adjustment.h
@@ -26,7 +26,7 @@
  * @ELVSS_OFF_THRESHOLD: Minimum backlight threshold for disable smart elvss
  * @EXPOSURE_ADJUSTMENT_MIN: Minimum available PCC coefficient for OLED panel
  */
-#define ELVSS_OFF_THRESHOLD        258
+#define ELVSS_OFF_THRESHOLD        266
 #define EXPOSURE_ADJUSTMENT_MIN    100
 
 /* PCC coefficient when exposure is 255 */
