9af61a0006dd893e9bd9cc6c5eda6a13fbe56e1e
Danny Lin <danny@kdrag0n.dev>
2019-03-13 20:20:46 -0700

    msm: camera: actuator: Do not boost on init if the display is off
    There's no point in minimizing camera init time when the display is off
    and the user is not interacting with the device. Block boosting in such
    scenarios just in case the camera is somehow initialized without
    explicit physical intervention from the user.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/media/platform/msm/camera/cam_sensor_module/cam_actuator/cam_actuator_core.c
+++ drivers/media/platform/msm/camera/cam_sensor_module/cam_actuator/cam_actuator_core.c
@@ -13,6 +13,7 @@
 #include <linux/module.h>
 #include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
+#include <linux/state_notifier.h>
 #include <cam_sensor_cmn_header.h>
 #include "cam_actuator_core.h"
 #include "cam_sensor_util.h"
@@ -504,8 +505,10 @@ int32_t cam_actuator_i2c_pkt_parse(struct cam_actuator_ctrl_t *a_ctrl,
 
 	switch (csl_packet->header.op_code & 0xFFFFFF) {
 	case CAM_ACTUATOR_PACKET_OPCODE_INIT:
-		cpu_input_boost_kick_max(500);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
+		if (!state_suspended) {
+			cpu_input_boost_kick_max(500);
+			devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
+		}
 
 		offset = (uint32_t *)&csl_packet->payload;
 		offset += (csl_packet->cmd_buf_offset / sizeof(uint32_t));
