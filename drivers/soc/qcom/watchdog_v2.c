b8a77eca1f4ef72f828ba4bc2e5e533c102e955d
Danny Lin <danny@kdrag0n.dev>
2019-03-23 23:31:58 -0700

    soc: qcom: watchdog_v2: Fix missing new lines after watchdog bite
    Fixes the following mess after a watchdog bite:
    
    [   38.882454] Wdog - STS: 0x1, CTL: 0x3, BARK TIME: 0x57fdf, BITE TIME: 0x1[   38.882463] Kernel panic - not syncing: Failed to cause a watchdog bite! - Falling back to kernel panic!
    ...
    [   42.686137] Causing a watchdog bite![   53.352782] Wdog - STS: 0x3, CTL: 0x3, BARK TIME: 0x47fe5, BITE TIME: 0x1
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/soc/qcom/watchdog_v2.c
+++ drivers/soc/qcom/watchdog_v2.c
@@ -496,7 +496,7 @@ void msm_trigger_wdog_bite(void)
 {
 	if (!wdog_data)
 		return;
-	pr_info("Causing a watchdog bite!");
+	pr_info("Causing a watchdog bite!\n");
 	__raw_writel(1, wdog_data->base + WDT0_BITE_TIME);
 	/* Mke sure bite time is written before we reset */
 	mb();
@@ -505,7 +505,7 @@ void msm_trigger_wdog_bite(void)
 	mb();
 	/* Delay to make sure bite occurs */
 	mdelay(10000);
-	pr_err("Wdog - STS: 0x%x, CTL: 0x%x, BARK TIME: 0x%x, BITE TIME: 0x%x",
+	pr_err("Wdog - STS: 0x%x, CTL: 0x%x, BARK TIME: 0x%x, BITE TIME: 0x%x\n",
 		__raw_readl(wdog_data->base + WDT0_STS),
 		__raw_readl(wdog_data->base + WDT0_EN),
 		__raw_readl(wdog_data->base + WDT0_BARK_TIME),
