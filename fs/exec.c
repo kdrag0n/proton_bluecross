kdrag0n/proton_bluecross

commit 7c5815fdd7feb98e1809b642331f378f84f71270

signal: Accelerate lmkd task killing speed
Make lmkd-killed tasks die faster by:
 - Boosting freshly killed tasks to a near-maximum priority with FIFO
 real-time schedling (credit: Sultan Alsawaf)
 - Affining freshly killed tasks to the big cluster (credit: Suren
 Baghdasaryan)
 - Boosting the system's CPU and DDR bus to their respective maximum
 frequencies when tasks are killed (credit: Sultan Alsawaf)
 - Invoking OOM reaper upon killing tasks to quickly reclaim memory by
 unmapping anonymous, file, and shared memory immediately (credit: Tim
 Murray)
 - Following the "fastpath" for signal sending, bypassing the signal
 queue (credit: Sultan Alsawaf)

These optimizations only apply to tasks killed by lmkd with the SIGKILL
signal.

Test: logging shows the code path being triggered
Test: minimum CPU frequency boosts to max when lmkd kills processes
Reviewed-by: Sultan Alsawaf <sultan@kerneltoast.com>
Signed-off-by: Danny Lin <danny@kdrag0n.dev>

fs/exec.c
@@ -74,13 +74,20 @@ static DEFINE_RWLOCK(binfmt_lock);
 
 #define ZYGOTE32_BIN	"/system/bin/app_process32"
 #define ZYGOTE64_BIN	"/system/bin/app_process64"
+#define LMKD_BIN	"/system/bin/lmkd"
 static atomic_t zygote32_pid;
 static atomic_t zygote64_pid;
+static atomic_t lmkd_pid;
 
 bool is_zygote_pid(pid_t pid)
 {
 	return atomic_read(&zygote32_pid) == pid ||
 		atomic_read(&zygote64_pid) == pid;
+}
+
+bool is_lmkd_pid(pid_t pid)
+{
+	return atomic_read(&lmkd_pid) == pid;
 }
 
 bool comm_should_block_write(char *comm)
@@ -1809,6 +1816,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 			atomic_set(&zygote32_pid, current->pid);
 		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
 			atomic_set(&zygote64_pid, current->pid);
+		else if (unlikely(!strcmp(filename->name, LMKD_BIN)))
+			atomic_set(&lmkd_pid, current->pid);
 	}
 
 	/* execve succeeded */

include/linux/binfmts.h
@@ -123,6 +123,7 @@ extern void install_exec_creds(struct linux_binprm *bprm);
 extern void set_binfmt(struct linux_binfmt *new);
 extern ssize_t read_code(struct file *, unsigned long, loff_t, size_t);
 extern bool is_zygote_pid(pid_t pid);
+extern bool is_lmkd_pid(pid_t pid);
 extern bool comm_should_block_write(char *comm);
 
 #endif /* _LINUX_BINFMTS_H */

kernel/signal.c
@@ -34,6 +34,10 @@
 #include <linux/compat.h>
 #include <linux/cn_proc.h>
 #include <linux/compiler.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
+#include <linux/display_state.h>
+#include <linux/oom.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/signal.h>
@@ -1337,16 +1341,69 @@ int __kill_pgrp_info(int sig, struct siginfo *info, struct pid *pgrp)
 	return success ? 0 : retval;
 }
 
+static void mark_lmkd_victim(struct task_struct *tsk)
+{
+	struct mm_struct *mm = tsk->mm;
+
+	if (!cmpxchg(&tsk->signal->oom_mm, NULL, mm)) {
+		atomic_inc(&tsk->signal->oom_mm->mm_count);
+		set_bit(MMF_OOM_VICTIM, &mm->flags);
+	}
+}
+
 int kill_pid_info(int sig, struct siginfo *info, struct pid *pid)
 {
+#ifdef CONFIG_MEMCG
+	static const struct sched_param rt_param = {
+		.sched_priority = MAX_RT_PRIO - 1
+	};
+#endif
 	int error = -ESRCH;
 	struct task_struct *p;
 
 	for (;;) {
 		rcu_read_lock();
 		p = pid_task(pid, PIDTYPE_PID);
-		if (p)
+		if (p) {
+#ifdef CONFIG_MEMCG
+			bool is_lmkd = false;
+
+			/* Accelerate lmkd task killing */
+			if (sig == SIGKILL && is_lmkd_pid(current->pid)) {
+				is_lmkd = true;
+				info = SEND_SIG_PRIV;
+				preempt_disable();
+
+				sched_setscheduler_nocheck(p, SCHED_FIFO,
+							   &rt_param);
+				set_cpus_allowed_ptr(p, cpu_perf_mask);
+
+				if (is_display_on()) {
+					cpu_input_boost_kick_max(250);
+					devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW,
+							       250);
+				}
+			}
+#endif
+
 			error = group_send_sig_info(sig, info, p);
+
+#ifdef CONFIG_MEMCG
+			if (is_lmkd) {
+				preempt_enable();
+
+				if (!error) {
+					task_lock(p);
+					if (p->mm && !test_bit(MMF_OOM_SKIP,
+					    &p->mm->flags)) {
+						mark_lmkd_victim(p);
+						wake_oom_reaper(p);
+					}
+					task_unlock(p);
+				}
+			}
+#endif
+		}
 		rcu_read_unlock();
 		if (likely(!p || error != -ESRCH))
 			return error;

commit 257c4b863a7f1357fa1360e63345f9ccd316d8c5

exec: Fix lmkd process tracking
When the code to track lmkd's PID instead of checking the value of its
`comm` field was added, it was never actually tested. This resulted in
the PID never being set, hence the lmkd kill speed optimizations were
never invoked. Fix it.

Fixes: 7c5815fdd7fe ("signal: Accelerate lmkd task killing speed")
Signed-off-by: Danny Lin <danny@kdrag0n.dev>

fs/exec.c
@@ -1815,8 +1815,8 @@ static int do_execveat_common(int fd, struct filename *filename,
 			zygote32_pid = current->pid;
 		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN)))
 			zygote64_pid = current->pid;
-		else if (unlikely(!strcmp(filename->name, LMKD_BIN)))
-			lmkd_pid = current->pid;
+	} else if (unlikely(!strcmp(filename->name, LMKD_BIN))) {
+		lmkd_pid = current->pid;
 	}
 
 	/* execve succeeded */