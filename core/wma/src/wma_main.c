8f24a52df88adad2cc125f6f0a35b4b2422c3149
Sunil Ravi <sunilravi@google.com>
2019-04-03 21:25:25 +0000

    Revert "qcacld-3.0: Clear PMK cache from driver"
    This reverts commit 90a6c098f0ea9b0cb2c2bcb010b9ead84e38b7e8.
    
    Reason for revert: Security team want to ensure the kernel for the June MPR only includes the triaged security/non-security changes. They are handling NIAP changes in a separate build, so they wanted to isolate those changes in a different branch.
    
    Change-Id: I2916fcd451b01cf18dae238b3efddaf07a9cd7c6
    Signed-off-by: Sunil Ravi <sunilravi@google.com>

--- core/wma/src/wma_main.c
+++ core/wma/src/wma_main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2019 The Linux Foundation. All rights reserved.
+ * Copyright (c) 2013-2018 The Linux Foundation. All rights reserved.
  *
  * Previously licensed under the ISC license by Qualcomm Atheros, Inc.
  *
@@ -8305,10 +8305,8 @@ QDF_STATUS wma_mc_process_msg(void *cds_context, cds_msg_t *msg)
 	case SIR_HAL_SET_DEL_PMKID_CACHE:
 		wma_set_del_pmkid_cache(wma_handle,
 			(wmi_pmk_cache *) msg->bodyptr, msg->reserved);
-		if (msg->bodyptr) {
-			qdf_mem_zero(msg->bodyptr, sizeof(wmi_pmk_cache));
+		if (msg->bodyptr)
 			qdf_mem_free(msg->bodyptr);
-		}
 		break;
 	case SIR_HAL_HLP_IE_INFO:
 		wma_roam_scan_send_hlp(wma_handle,
