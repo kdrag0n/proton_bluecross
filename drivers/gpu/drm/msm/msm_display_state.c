4f5e410dfca06f51e8f2b25a93543752268169f9
Danny Lin <danny@kdrag0n.dev>
2019-04-05 21:43:13 -0700

    drm/msm: Expose display state as a sysfs attribute
    Instead of using the relatively limited module parameter system,
    register a sysfs attribute directly. This will provide more flexibility
    in how we handle the value of this attribute.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/gpu/drm/msm/msm_display_state.c
+++ drivers/gpu/drm/msm/msm_display_state.c
@@ -7,16 +7,35 @@
 
 #define pr_fmt(fmt) "display_state: " fmt
 
+#include <linux/kobject.h>
+#include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/msm_drm_notify.h>
+#include <linux/sysfs.h>
 
+static struct kobject *module_kobj;
 static bool display_on __read_mostly = true;
-module_param(display_on, bool, 0444);
 
 bool is_display_on(void)
 {
 	return display_on;
 }
+
+static ssize_t display_state_show(struct kobject *kobj,
+				  struct kobj_attribute *attr, char *buf)
+{
+	return snprintf(buf, PAGE_SIZE, "%u\n", display_on);
+}
+static struct kobj_attribute display_state_attr = __ATTR_RO(display_state);
+
+static struct attribute *attrs[] = {
+	&display_state_attr.attr,
+	NULL
+};
+
+static const struct attribute_group attr_group = {
+	.attrs = attrs,
+};
 
 static int msm_drm_notifier_cb(struct notifier_block *nb, unsigned long action,
 			       void *data)
@@ -43,9 +62,30 @@ static int __init display_state_init(void)
 	int ret;
 
 	ret = msm_drm_register_client(&display_state_nb);
-	if (ret)
+	if (ret) {
 		pr_err("Failed to register msm_drm notifier, err: %d\n", ret);
+		return ret;
+	}
 
+	module_kobj = kset_find_obj(module_kset, KBUILD_MODNAME);
+	if (!module_kobj) {
+		pr_err("Failed to locate module kobject\n");
+		ret = -ENOENT;
+		goto err_unreg_drm_notif;
+	}
+
+	ret = sysfs_create_group(module_kobj, &attr_group);
+	if (ret) {
+		pr_err("Failed to create sysfs group, err: %d\n", ret);
+		goto err_put_kobj;
+	}
+
+	return 0;
+
+err_unreg_drm_notif:
+	msm_drm_unregister_client(&display_state_nb);
+err_put_kobj:
+	kobject_put(module_kobj);
 	return ret;
 }
 late_initcall(display_state_init);
