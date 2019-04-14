7313d952ecb45c5e998943a6c13ec0caf9f9e9a0
Danny Lin <danny@kdrag0n.dev>
2019-04-13 21:08:17 -0700

    msm: pcie: Mark MSI cascade ISR as IRQF_NO_THREAD
    This should help make forced IRQ threading practical on systems that use
    pci-msm, similar to commit 8ff0ef996ca0 ("PCI: host: Mark PCIe/PCI (MSI)
    IRQ cascade handlers as IRQF_NO_THREAD").
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- drivers/pci/host/pci-msm.c
+++ drivers/pci/host/pci-msm.c
@@ -5207,7 +5207,7 @@ static int32_t msm_pcie_irq_init(struct msm_pcie_dev_t *dev)
 		rc = devm_request_irq(pdev,
 			dev->irq[MSM_PCIE_INT_MSI].num,
 			handle_msi_irq,
-			IRQF_TRIGGER_RISING,
+			IRQF_TRIGGER_RISING | IRQF_NO_THREAD,
 			dev->irq[MSM_PCIE_INT_MSI].name,
 			dev);
 		if (rc) {
