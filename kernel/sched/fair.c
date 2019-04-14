b001b2f847b6e38d11791341ee49f1b8557ba6aa
Danny Lin <danny@kdrag0n.dev>
2019-04-13 23:48:06 -0700

    Revert "ANDROID: sched/fair: unify spare capacity calculation"
    This reverts commit f26c3db141f1350fbfe7e7e29a2b34d57623f385.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6952,7 +6952,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	int best_idle_cpu = -1;
 	int target_cpu = -1;
 	int cpu, i;
-	long most_spare_cap = 0;
+	long spare_cap, most_spare_cap = 0;
 	int most_spare_cap_cpu = -1;
 	unsigned int active_cpus_count = 0;
 	int isolated_candidate = -1;
@@ -6997,7 +6997,6 @@ retry:
 			unsigned long capacity_curr = capacity_curr_of(i);
 			unsigned long capacity_orig = capacity_orig_of(i);
 			unsigned long wake_util, new_util;
-			long spare_cap;
 
 			cpumask_clear_cpu(i, &search_cpus);
 
@@ -7035,13 +7034,6 @@ retry:
 			new_util = max(min_util, new_util);
 			if (new_util > capacity_orig)
 				continue;
-
-			/*
-			 * Pre-compute the maximum possible capacity we expect
-			 * to have available on this CPU once the task is
-			 * enqueued here.
-			 */
-			spare_cap = capacity_orig - new_util;
 
 			/*
 			 * Case A) Latency sensitive tasks
@@ -7094,9 +7086,9 @@ retry:
 				 * Case A.2: Target ACTIVE CPU
 				 * Favor CPUs with max spare capacity.
 				 */
-				if (capacity_curr > new_util &&
-				    spare_cap > target_max_spare_cap) {
-					target_max_spare_cap = spare_cap;
+				if ((capacity_curr > new_util) &&
+					(capacity_orig - new_util > target_max_spare_cap)) {
+					target_max_spare_cap = capacity_orig - new_util;
 					target_cpu = i;
 					continue;
 				}
@@ -7208,10 +7200,10 @@ retry:
 
 			/* Favor CPUs with maximum spare capacity */
 			if (capacity_orig == target_capacity &&
-			    spare_cap < target_max_spare_cap)
+			    (capacity_orig - new_util) < target_max_spare_cap)
 				continue;
 
-			target_max_spare_cap = spare_cap;
+			target_max_spare_cap = capacity_orig - new_util;
 			target_capacity = capacity_orig;
 			target_util = new_util;
 			target_cpu = i;
