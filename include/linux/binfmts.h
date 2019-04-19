0bfb31e01a2ea61c02a695f8909220d87241fe4b
Danny Lin <danny@kdrag0n.dev>
2019-04-18 22:49:31 -0700

    exec: Rename boost task check function to match most task checks
    Rename the function from tsk_is_booster to task_is_booster since there
    are more functions following this naming scheme throughout kernel.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -125,7 +125,7 @@ extern ssize_t read_code(struct file *, unsigned long, loff_t, size_t);
 extern bool task_is_zygote(struct task_struct *p);
 extern bool task_is_lmkd(struct task_struct *p);
 
-static inline bool tsk_is_booster(struct task_struct *tsk)
+static inline bool task_is_booster(struct task_struct *tsk)
 {
 	char comm[sizeof(tsk->comm)];
 
