44d62bdac71af0d9bad60b2a46ee7c17c5ca19b4
Danny Lin <danny@kdrag0n.dev>
2019-05-01 22:24:43 -0700

    init: Bump SchedTune boost levels
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- flasher/overlay/init.proton.sh
+++ flasher/overlay/init.proton.sh
@@ -48,7 +48,7 @@ case "$1" in
 		gov_param up_rate_limit_us 5000
 		gov_param down_rate_limit_us 5000
 		# CPU: Boost
-		stune_boost 10
+		stune_boost 15
 		boost_duration 32
 		boost_timeout 1750
 
@@ -75,7 +75,7 @@ case "$1" in
 		gov_param up_rate_limit_us 500
 		gov_param down_rate_limit_us 20000
 		# CPU: Boost
-		stune_boost 15
+		stune_boost 50
 		boost_duration 64
 		boost_timeout 3250
 
@@ -101,7 +101,7 @@ case "$1" in
 		big_gov_param up_rate_limit_us 500
 		big_gov_param down_rate_limit_us 25000
 		# CPU: Boost
-		stune_boost 1
+		stune_boost 60
 		boost_duration 125
 		boost_timeout 15000
 
@@ -127,7 +127,7 @@ case "$1" in
 		big_gov_param up_rate_limit_us 400
 		big_gov_param down_rate_limit_us 25000
 		# CPU: Boost
-		stune_boost 1
+		stune_boost 60
 		boost_duration 125
 		boost_timeout 45000
 
